5caf8812adc0c843c2291d81f9ef18b0bb752dbd
parksw3 <parksw3@mcmaster.ca>
2022-10-18 03:56:20 -0400

    done
    Former-commit-id: 4ec038d506ccdb66b34fc82bf3163585eb201981

--- figure_exponential.R
+++ figure_exponential.R
@@ -2,7 +2,11 @@ library(brms)
 library(dplyr)
 library(ggplot2); theme_set(theme_bw(base_family="Times"))
 source("param.R")
+source("correct.R")
 load("fit_exponential.rda")
+
+r <- seq(-0.2, 0.2, length.out=11)
+truemean <- exp(logmean + logsd^2/2)
 
 organize <- function(x) {
   x <- as.data.frame(x)
@@ -17,27 +21,55 @@ organize <- function(x) {
     )
 }
 
-ff_naive <- fixef(naive_model) %>%
-  organize %>%
+## not subsampling despite its name...
+
+ff_naive <- naive_model %>%
+   dcorrect(r=r, subsample=4000) %>%
   mutate(
     fit="Naive"
   )
 
-ff_rtrunc <- fixef(rtrunc_model) %>%
-  organize %>%
+ff_dynamic <- naive_model %>%
+  dcorrect(r=r, subsample=4000, correct=TRUE) %>%
+  mutate(
+    fit="Dynamical correction"
+  )
+
+ff_rtrunc <- rtrunc_model %>%
+  dcorrect(r=r, subsample=4000) %>%
   mutate(
     fit="Right truncation"
   )
 
+ff_rtrunc_dynamic <- rtrunc_model %>%
+  dcorrect(r=r, subsample=4000, correct=TRUE) %>%
+  mutate(
+    fit="Right truncation + dynamical correction"
+  )
+
+ff_ltrunc_dynamic <- ltrunc_model %>%
+  dcorrect(r=r, subsample=4000, correct=TRUE) %>%
+  mutate(
+    fit="Left truncation + dynamical correction"
+  )
+
 ff_all <- bind_rows(
   ff_naive,
-  ff_rtrunc
+  ff_dynamic,
+  ff_rtrunc,
+  ff_rtrunc_dynamic,
+  ff_ltrunc_dynamic
 )
 
 g1 <- ggplot(ff_all) +
-  geom_point(aes(r, Estimate, col=fit, shape=fit), position=position_dodge(0.01)) +
-  geom_errorbar(aes(r, ymin=Q2.5, ymax=Q97.5, col=fit), width=0, position=position_dodge(0.01)) +
-  scale_color_viridis_d(end=0.8)
+  geom_point(aes(r, estimate, col=fit, shape=fit), position=position_dodge(0.015)) +
+  geom_errorbar(aes(r, ymin=lwr, ymax=upr, col=fit), width=0, position=position_dodge(0.015)) +
+  geom_hline(yintercept=truemean, lty=2) +
+  scale_y_log10() +
+  scale_color_viridis_d(end=0.8) +
+  theme(
+    legend.position = "top"
+  )
 
-ggsave("figure_exponential.pdf", g1, width=6, height=4)
-ggsave("figure_exponential.png", g1, width=6, height=4)
+ggsave("figure_exponential.pdf", g1, width=10, height=8)
+ggsave("figure_exponential.png", g1, width=10, height=8)
