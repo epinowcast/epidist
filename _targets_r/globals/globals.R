816808e2c652082ec63ae11b4c3041666b5c1d02
Sam Abbott <contact@samabbott.co.uk>
2022-10-24 20:57:14 +0000

    add targets structure and initial scenario scaffolding
    Former-commit-id: 64d39f848662a73e6ede2c22f8fed8d5da3eb002

---
+++ _targets_r/globals/globals.R
@@ -0,0 +1,22 @@
+library(targets)
+library(tarchetypes)
+library(cmdstanr)
+library(data.table)
+library(ggplot2)
+library(purrr, quietly = TRUE)
+library(here)
+library(future)
+library(future.callr)
+plan(callr)
+functions <- list.files(here("R"), full.names = TRUE)
+walk(functions, source)
+rm("functions")
+set_cmdstan_path()  
+tar_option_set(
+  packages = c("data.table", "ggplot2", "purrr", "cmdstanr", "here"),
+  deployment = "worker",
+  memory = "transient",
+  workspace_on_error = TRUE,
+  error = "continue",
+  garbage_collection = TRUE
+)
