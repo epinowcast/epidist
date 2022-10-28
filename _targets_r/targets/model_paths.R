01d753483ed7dec6f695b6e40efb383fa9ae3714
Sam Abbott <contact@samabbott.co.uk>
2022-10-28 11:19:27 +0000

    add handling of model compilation
    Former-commit-id: f760981df4933f4b7dc970e7ff0dafe90fca8183

--- _targets_r/targets/model_paths.R
+++
@@ -1,9 +0,0 @@
-tar_target(
-  model_paths,
-  paste0(
-    "data/models/", gsub(" ", "_", tolower(names(models))), ".stan"
-  ) |>
-    fs::file_create(),
-  format = "file",
-  pattern = map(models)
-)
