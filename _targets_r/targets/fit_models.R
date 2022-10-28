0883582e792f021cf5435bf35b19e4ee7e9d6280
Sam Abbott <contact@samabbott.co.uk>
2022-10-28 17:12:41 +0000

    add model fitting postprocessing

--- _targets_r/targets/fit_models.R
+++ _targets_r/targets/fit_models.R
@@ -45,5 +45,30 @@ tar_map(
         seed = 123
       ),
     pattern = map(standata, scenarios)
+  ),
+  tar_target(
+    diagnostics,
+    fit[, fit := NULL]
+  ),
+  tar_file(
+    save_diagnostics,
+    save_csv(diagnostics, paste0("data/diagnostics/", model_name, '.csv'))
+  ),
+  tar_target(
+    draws,
+    extract_lognormal_draws(fit, scenarios),
+    pattern = map(fit, scenarios)
+  ),
+  tar_file(
+    save_lognormal_draws,
+    save_csv(draws, paste0("data/posteriors/", model_name, '.csv'))
+  ),
+  tar_target(
+    summarised_draws,
+    summarise_lognormal_draws(draws)
+  ),
+  tar_file(
+    save_summarised_draws,
+    save_csv(summarised_draws, paste0("data/summarise_posteriors/", model_name, '.csv'))
   )
 )
