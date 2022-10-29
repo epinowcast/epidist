9d1b627dc1fe1193bac112cee9541ade8987f6c0
Daniel <parksw3@mcmaster.ca>
2022-10-29 15:42:22 -0400

    first proper results

--- _targets_r/targets/distributions.R
+++ _targets_r/targets/distributions.R
@@ -1,9 +1,9 @@
 tar_group_by(
   distributions,
     data.table(
-    scenario = c("short", "medium", "long"),
-    meanlog = c(1.2, 1.6, 2),
-    sdlog = c(0.4, 0.6, 0.8)
+    scenario = c("short"),
+    meanlog = c(1.2),
+    sdlog = c(0.4)
   ) |>
     DT(, mean := exp(meanlog + sdlog^2/2)) |>
     DT(, sd := exp(meanlog + (1/2)*sdlog^2)*sqrt(exp(sdlog^2) - 1)),
