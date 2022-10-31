c3eeeb7489baefdd48b90f7d1b3efb737eb2e47e
Sam Abbott <contact@samabbott.co.uk>
2022-10-31 15:27:54 +0000

    add basic functions for plotting obs
    Former-commit-id: 918c105477335e7e7c3190e6075637d480ede84a

--- R/plot.R
+++ R/plot.R
@@ -15,3 +15,24 @@ plot_relative_recovery <- function(relative_data, alpha = 0.8,
     theme_bw() +
     labs(x = "Relative value")
 }
+
+plot_cases_by_obs_window <- function(cases) {
+  cases |>
+    DT(case_type == "primary") |>
+    ggplot() +
+    aes(x = time, y = cases) +
+    geom_col(aes(fill = factor(obs_at)), alpha = 1, col = "#696767b1") +
+    geom_point(
+      data = truncated_cases[case_type == "secondary"],
+      col = "#292828b1"
+    ) +
+    geom_vline(
+      aes(xintercept = as.numeric(as.character(obs_at))),
+      linetype = 2, alpha = 0.9
+    ) +
+    scale_fill_brewer(palette = "Blues", direction = 1) +
+    theme_bw() +
+    theme(legend.position = "bottom") +
+    labs(x = "Days", y = "Cases") + 
+    guides(fill = guide_legend(title = "Observation day"))
+}
