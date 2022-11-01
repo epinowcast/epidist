45f45da583570fff6b6acc39f98f0ea8b1421fa6
Sam Abbott <contact@samabbott.co.uk>
2022-11-01 17:52:36 +0000

    obeying the grand linter
    Former-commit-id: 7e233dcc58eed1e65e2675a03224267a01067a08 [formerly f5be8535cac7f2e4552b20d02ceb871da205393d]
    Former-commit-id: 2899bc698dcb30d1532758f4972f681efc0f9e13
    Former-commit-id: 718186a3cc6d7e138c4c2d5925cc41c1ee4a6c9a

--- R/apply_dynamic_correction.R
+++ R/apply_dynamic_correction.R
@@ -5,46 +5,46 @@
 #' @export
 apply_dynamic_correction <- function(x, r, subsample, correct = FALSE) {
   pp <- posterior_samples(x)
-  
+
   npost <- nrow(pp)
-  
-  ss <- sample(1:npost, subsample, replace=FALSE)
-  
-  reslist <- vector('list', length(r))
-  
+
+  ss <- sample(1:npost, subsample, replace = FALSE)
+
+  reslist <- vector("list", length(r))
+
   for (j in 1:length(r)) {
     meanvec <- rep(NA, length(ss))
-    
+
     for (i in ss) {
       logmean <- pp[i, j]
       logsd <- exp(pp[i, j+length(r)])
-      
+
       if (correct) {
-        
+
         ii1 <- integrate(function(z) {
-          dlnorm(z, meanlog=logmean, sdlog=logsd) * exp(r[j] * z)
+          dlnorm(z, meanlog = logmean, sdlog = logsd) * exp(r[j] * z)
         }, 0, 100, subdivisions = 100L)[[1]]
-        
+
         ii2 <- integrate(function(z) {
-          dlnorm(z, meanlog=logmean, sdlog=logsd) * exp(r[j] * z) * z
+          dlnorm(z, meanlog = logmean, sdlog = logsd) * exp(r[j] * z) * z
         }, 0, 100, subdivisions = 100L)[[1]]
-        
-        meanvec[match(i, ss)] <- ii2/ii1
+
+        meanvec[match(i, ss)] <- ii2 / ii1
       } else {
-        meanvec[match(i, ss)] <- exp(logmean + logsd^2/2)
+        meanvec[match(i, ss)] <- exp(logmean + logsd^2 / 2)
       }
-      
+
     }
-    
+
     reslist[[j]] <- data.frame(
-      estimate=mean(meanvec),
-      lwr=quantile(meanvec, 0.025),
-      upr=quantile(meanvec, 0.975),
-      r=r[j]
+      estimate = mean(meanvec),
+      lwr = quantile(meanvec, 0.025),
+      upr = quantile(meanvec, 0.975),
+      r = r[j]
     )
   }
-  
+
   out <- do.call("rbind", reslist)
-  
+
   out
 }
