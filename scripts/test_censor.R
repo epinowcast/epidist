dcf6330cf78608f4ea478c90d8b044659865af24
Daniel <parksw3@mcmaster.ca>
2022-11-02 02:41:01 -0400

    one more test
    Former-commit-id: 1f054518555096145cb29eb0d1b9a3aa7e25f992
    Former-commit-id: 52035bf69c33ac82ef46d0edc907938747c8213b
    Former-commit-id: 421d3e129ab6bde92b2dfd855662cb5bcb303e01 [formerly e1125c8ac4ce008efe6d74be88ba20606ab599d4]
    Former-commit-id: ab75324af23d040e5db6ac490323051fcb980cb7

---
+++ scripts/test_censor.R
@@ -0,0 +1,73 @@
+library(dynamicaltruncation)
+library(data.table)
+library(purrr)
+library(ggplot2)
+
+tfun <- function(r=0.2, t=1) {
+  (exp(r * t) * (r * t - 1) + 1)/(r * (exp(r * t) - 1))
+  
+}
+
+outbreak <- simulate_exponential_cases(sample_size = 100000)
+
+secondary_dist <- data.table(
+  meanlog = 1.8, sdlog = 0.5
+) |>
+  add_natural_scale_mean_sd()
+
+obs <- outbreak |>
+  simulate_secondary(
+    meanlog = secondary_dist$meanlog[[1]],
+    sdlog = secondary_dist$sdlog[[1]]
+  ) |>
+  observe_process()
+
+cases <- construct_cases_by_obs_window(
+  obs, windows = c(100)
+)
+
+plot_cases_by_obs_window(cases) +
+  scale_y_log10()
+
+censor_delay1 <- dynamicaltruncation:::calculate_censor_delay(obs)
+
+dynamicaltruncation:::plot_censor_delay(censor_delay1) +
+  geom_hline(yintercept=tfun(), lty=2)
+
+truncated_obs <- obs |>
+  filter_obs_by_obs_time(obs_time = 30)
+
+
+censor_delay <- dynamicaltruncation:::calculate_censor_delay(truncated_obs)
+
+dynamicaltruncation:::plot_censor_delay(censor_delay)  +
+  geom_hline(yintercept=tfun(), lty=2)
+
+cases2 <- construct_cases_by_obs_window(
+  obs, windows = c(30)
+)
+
+plot_cases_by_obs_window(cases2)
+
+outbreak2 <- simulate_exponential_cases(r=0, sample_size = 100000)
+
+obs2 <- outbreak2 |>
+  simulate_secondary(
+    meanlog = secondary_dist$meanlog[[1]],
+    sdlog = secondary_dist$sdlog[[1]]
+  ) |>
+  observe_process()
+
+truncated_obs2 <- obs2 |>
+  filter_obs_by_obs_time(obs_time = 30)
+
+
+censor_delay2 <- dynamicaltruncation:::calculate_censor_delay(truncated_obs2)
+
+dynamicaltruncation:::plot_censor_delay(censor_delay2)
+
+
+censor_delay2 <- dynamicaltruncation:::calculate_censor_delay(obs2)
+
+dynamicaltruncation:::plot_censor_delay(censor_delay2)
+
