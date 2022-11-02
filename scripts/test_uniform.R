cc303349a331cfaec115cc37d9717f9aee673df3
Sam Abbott <contact@samabbott.co.uk>
2022-11-02 13:44:05 +0000

    I am a busy man who can't be doing all this compilation
    Former-commit-id: 96213ac8441e58849846091345406167f7a8794e
    Former-commit-id: d78e738862233e1b7915d8fba703fed1e3d38e86
    Former-commit-id: fda2ea310e2d97547bf78ce0ee930839fd1b4ab4 [formerly a0b3c6e0a05049def80504325076d4a18eccdf4a]
    Former-commit-id: 80ce0701e6592c93cffd02b92ec1636f1155a938

--- scripts/test_uniform.R
+++ scripts/test_uniform.R
@@ -19,23 +19,38 @@ obs <- outbreak |>
   ) |>
   observe_process()
 
+truncated_obs <- obs |>
+    filter_obs_by_obs_time(obs_time = 25)
+
+model_path <- file.path(tempdir(), "model.stan")
+model_code <- latent_truncation_censoring_adjusted_delay(
+  data = truncated_obs, fn = brms::make_stancode,
+  save_model = model_path
+)
+compiled_model <- cmdstanr::cmdstan_model(model_path)
+
 fitlist <- lapply(1:nrep, function(x) {
   set.seed(x)
-  truncated_obs <- obs |>
-    filter_obs_by_obs_time(obs_time = 25) |>
+  sample_obs <- truncated_obs |>
     DT(sample(1:.N, 200, replace = FALSE))
-  
-  latent_truncation_censoring_fit <- latent_truncation_censoring_adjusted_delay(
-    data = truncated_obs, cores = 4, refresh = 0
+
+  data <- latent_truncation_censoring_adjusted_delay(
+    data = sample_obs, fn = brms::make_standata
   )
-  
-  latent_truncation_censoring_fit
-}) 
+  fit <- sample_model(
+    model = model_path,
+    data = data,
+    diagnostics = TRUE,
+    cores = 4,
+    refresh = 0
+  )
+  return(fit[])
+})
 
 names(fitlist) <- paste0("fit", 1:20)
 
 draws <- fitlist |>
-  map(extract_lognormal_draws) |>
+  map(extract_lognormal_draws, from_dt = TRUE) |>
   map(draws_to_long) |>
   rbindlist(idcol = "model")
 
@@ -50,9 +65,9 @@ draws |>
   )
 
 summ <- draws |>
-  DT(, .(mean=mean(value),
-         lwr=quantile(value, 0.025),
-         upr=quantile(value, 0.975)), by=c("model", "parameter"))
+  DT(, .(mean = mean(value),
+         lwr = quantile(value, 0.025),
+         upr = quantile(value, 0.975)), by = c("model", "parameter"))
 
 
-save(summ, file="test_uniform.rda")
+save(summ, file = "test_uniform.rda")
