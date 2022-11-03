8ea6bbb1a503984dfafde40a90bcc611672c0ec1
Sam Abbott <contact@samabbott.co.uk>
2022-11-03 17:39:28 +0000

    fix scenario binding
    Former-commit-id: f10046235e228c7ea3a7b5d621261fdf9e3ce7ed [formerly 5ab7d7f9cf9831f6dba637ed5f770fb3fe44d029]
    Former-commit-id: fc451ac3705548a2e38204e2ce491405bbc575ed

--- _targets_r/targets/scenarios.R
+++ _targets_r/targets/scenarios.R
@@ -1,5 +1,7 @@
 tar_target(scenarios, {
-  rbind(simulated_scenarios_outbreak, simulated_scenarios_exponential) |> 
+  rbind(simulated_scenarios_outbreak[, replicate := 1],
+        simulated_scenarios_exponential
+  ) |> 
     as.data.table() |> 
     DT(, id := 1:.N)
 })
