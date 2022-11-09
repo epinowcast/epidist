4e341383efbdc7045e658c58d95bbcc1b662cdaf
Daniel <parksw3@mcmaster.ca>
2022-11-08 23:14:56 -0500

    some more tests

--- scripts/test_uniform_censor_prior.R
+++ scripts/test_uniform_censor_prior.R
@@ -27,31 +27,13 @@ truncated_obs <- obs |>
 
 censor_delay <- calculate_censor_delay(truncated_obs)
 
-## looks pretty uniform to me
-plot_censor_delay(censor_delay)
-g1 <- ggplot(truncated_obs) +
-  geom_line(aes(ptime - ptime_daily, qunif(ptime - ptime_daily))) +
-  theme_bw()
-
-g2 <- ggplot(truncated_obs) +
-  geom_line(aes(stime - stime_daily, qunif(stime - stime_daily))) +
-  theme_bw()
-
-egg::ggarrange(g1, g2, nrow=1)
-
 latent_model_path <- file.path(tempdir(), "latent_model.stan")
 latent_model_code <- latent_censoring_adjusted_delay(
   data = truncated_obs, fn = brms::make_stancode,
-  save_model = latent_model_path
+  save_model = latent_model_path,
+  prior=brms::prior(normal(1.8, 10), class="Intercept") + brms::prior(normal(-0.7, 10), class="Intercept", dpar="sigma")
 )
 latent_compiled_model <- cmdstanr::cmdstan_model(latent_model_path)
-
-censor_model_path <- file.path(tempdir(), "censor_model.stan")
-censor_model_code <- censoring_adjusted_delay(
-  data = truncated_obs, fn = brms::make_stancode,
-  save_model = censor_model_path
-)
-censor_compiled_model <- cmdstanr::cmdstan_model(censor_model_path)
 
 fitlist <- lapply(1:nrep, function(x) {
   message("Starting rep: ", x)
@@ -60,10 +42,6 @@ fitlist <- lapply(1:nrep, function(x) {
     DT(sample(1:.N, 200, replace = FALSE))
 
   latent_data <- latent_censoring_adjusted_delay(
-    data = sample_obs, fn = brms::make_standata
-  )
-  
-  censor_data <- censoring_adjusted_delay(
     data = sample_obs, fn = brms::make_standata
   )
   
@@ -76,65 +54,58 @@ fitlist <- lapply(1:nrep, function(x) {
     show_messages = FALSE
   )
   
-  censor_fit <- sample_model(
-    model = censor_model_path,
-    data = censor_data,
-    diagnostics = TRUE,
-    parallel_chains = 4,
-    refresh = 0,
-    show_messages = FALSE
-  )
-  
-  return(list(latent=latent_fit[], censor=censor_fit[]))
+  return(latent_fit[])
 })
 
 names(fitlist) <- paste0("fit", 1:nrep)
 
 draws <- fitlist |>
-  map(function(x) {
-    x |> 
-      map(extract_lognormal_draws, from_dt = TRUE) |>
-      map(draws_to_long) |>
-      rbindlist(idcol = "fit")
-  }) |>
-  rbindlist(idcol="model")
+  map(extract_lognormal_draws, from_dt = TRUE) |>
+  map(draws_to_long) |>
+  rbindlist(idcol = "model")
 
 relative_draws <- draws |>
-  make_relative_to_truth(secondary_dist)
+  make_relative_to_truth(secondary_dist) 
+
+relative_bias <- relative_draws |>
+  DT(parameter=="sdlog") |>
+  DT(, .(bias = mean(rel_value)), by="model")
+
+mean(relative_bias$bias)
 
 scores <- relative_draws |>
   copy() |>
-  DT(, sample := 1:.N, by = c("model", "fit", "parameter")) |>
+  DT(, sample := 1:.N, by = c("model", "parameter")) |>
   DT(, prediction := value) |>
   DT(, value := NULL) |>
-  DT(, .(model, fit, parameter, prediction, true_value, sample)) |>
+  DT(, .(model, parameter, prediction, true_value, sample)) |>
   score() |>
-  summarise_scores(by = c("parameter", "fit")) |>
+  summarise_scores(by = c("parameter")) |>
   summarise_scores(fun = signif, digits = 2)
 
 scores
 
 relative_draws |>
-  plot_relative_recovery(fill=fit) +
+  plot_relative_recovery() +
   facet_wrap(vars(parameter), nrow = 1, scales = "free_x") +
   # guides(fill = guide_none()) +
   labs(
     y = "Fits", x = "Relative to ground truth"
   )
 
-#summ <- draws |>
-#  DT(, .(mean = mean(value),
-#         lwr = quantile(value, 0.025),
-#         upr = quantile(value, 0.975)), by = c("model", "parameter")
-#  )
+summ <- draws |>
+  DT(, .(mean = mean(value),
+         lwr = quantile(value, 0.025),
+         upr = quantile(value, 0.975)), by = c("model", "parameter")
+  )
+
+mean(summ[parameter=="sdlog"]$mean)
 
 # save(summ, file = "test_uniform.rda")
 
 summ <- fitlist |>
   map(function(x) {
-    lf <- x[[1]]
-    
-    dd <- lf$fit[[1]]
+    dd <- x$fit[[1]]
     
     ss <- dd$summary()
     
