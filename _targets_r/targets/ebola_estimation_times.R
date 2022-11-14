0d8b98adb12a4b09bd56f35f269e052629ff0d30
seabbs <s.e.abbott12@gmail.com>
2022-11-14 19:11:28 +0000

    check pipeline
    Former-commit-id: b8c0dbebbcb96d0d80e55cd6d8f7097fed3c0ac7 [formerly d1829a32a75a9553a444fb0e2a5e3d1e06128672]
    Former-commit-id: 1f65937a5ea82b2c577016e9e0e6fc50b8ea9cbf

--- _targets_r/targets/ebola_estimation_times.R
+++ _targets_r/targets/ebola_estimation_times.R
@@ -1,7 +1,7 @@
 tar_group_by(
   ebola_estimation_times,
   data.table(
-    scenario = c("60 days", "120 days", "180 days", "240 days"),
+    scenario = as.factor(c("60 days", "120 days", "180 days", "240 days")),
     time = c(60, 120, 180, 240)
   ),
   scenario
