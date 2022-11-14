ec6a0118978dca8e37572ce317d08cc83807aa39
seabbs <s.e.abbott12@gmail.com>
2022-11-14 14:57:21 +0000

    add retrospective cohorts for case study
    Former-commit-id: d0da7dd433dbee9ae06934e210e42956ee714c20 [formerly 53098dcd42b04e0fa0d03ebfd1a1ca198fb8f4ff]
    Former-commit-id: 004894e7e81e66c017af0029976f0a3795e43261
    Former-commit-id: 4ec98cd6d45ce3a6ca6b8b2d78ab64250757ae7a

--- _targets_r/targets/retro_ebola_obs.R
+++ _targets_r/targets/retro_ebola_obs.R
@@ -1,10 +1,10 @@
 tar_target(
-  truncated_ebola_obs,
+  retrospective_ebola_obs,
   case_study_data |>
-    filter_obs_by_obs_time(
+    filter_obs_by_ptime(
       obs_time = ebola_estimation_times[, "time"][[1]]
     ) |>
     DT(, scenario := ebola_estimation_times[, "scenario"][[1]]) |>
-    DT(, obs_type := "real-time"),
+    DT(, obs_type := "retrospective"),
   pattern = map(ebola_estimation_times)
 )
