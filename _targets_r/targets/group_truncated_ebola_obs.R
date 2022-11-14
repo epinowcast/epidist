1a7eb5ad3fa421b7bf4e62e414e575c4f23fd8c7
seabbs <s.e.abbott12@gmail.com>
2022-11-14 19:11:28 +0000

    check pipeline

--- _targets_r/targets/group_truncated_ebola_obs.R
+++ _targets_r/targets/group_truncated_ebola_obs.R
@@ -1,5 +1,5 @@
 tar_group_by(
   group_truncated_ebola_obs,
-  rbindlist(truncated_ebola_obs, retrospective_ebola_obs),
+  rbind(truncated_ebola_obs, retrospective_ebola_obs),
   scenario, obs_type
 )
