0cde070b5e2f0e76b2cb1485ee6ff5d713a9c71d
Sam <s.e.abbott12@gmail.com>
2023-02-06 18:41:00 +0000

    add case study plot and highlight issues with retrospective fits
    Former-commit-id: 75710ce3c5e244315e989b1552e6cc366b6035bb
    Former-commit-id: f9770f993c163d121c32bb97b6c887e3d165d99c
    Former-commit-id: 844c99dec9e8e377030adcd0f6fed4bba9de0797 [formerly a5c53dd7d088635fc6299e7edfd6654a1ad0670c]
    Former-commit-id: 3a19fd41f2c9b09d1e0e84cd3ef0c46c5bd90cfa

--- R/plot.R
+++ R/plot.R
@@ -1,20 +1,27 @@
+#' Plot the posterior estimates as densities
+#' @export
+plot_recovery <- function(data, alpha = 0.8,
+                                   quantiles = c(0.05, 0.35, 0.65, 0.95), ...) {
+  data |>
+    ggplot() +
+    aes(x = value, ...) +
+    ggridges::geom_density_ridges(
+      scale = 1.5, alpha = alpha, quantile_lines = TRUE,
+      quantiles = quantiles
+    ) +
+    theme_bw()
+}
+
 #' Plot the relative difference between true values and posterior estimates
 #' @export
 plot_relative_recovery <- function(relative_data, alpha = 0.8,
                                    quantiles = c(0.05, 0.35, 0.65, 0.95), ...) {
   relative_data |>
-    ggplot() +
-    aes(x = rel_value, ...) +
-    ggridges::geom_density_ridges(
-      scale = 1.5, alpha = alpha, quantile_lines = TRUE,
-      quantiles = quantiles
-    ) +
-    scale_x_continuous(
-      trans = "log", limits = c(NA, NA),
-      labels = ~ scales::comma(.x, accuracy = 0.1),
+    plot_recovery(
+      data = copy(relative_data)[, value := rel_value],
+      alpha = alpha, quantiles = quantiles, ...
     ) +
     geom_vline(xintercept = 1, linetype = 2, size = 1.05, alpha = 0.8) +
-    theme_bw() +
     labs(x = "Relative value")
 }
 
