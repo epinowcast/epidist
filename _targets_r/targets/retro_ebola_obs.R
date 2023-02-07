c6209e879a82fa2842675c0b20f9e0945a80a4e1
Sam <s.e.abbott12@gmail.com>
2023-02-07 09:50:14 +0000

    correct construction of retrospective observations
    Former-commit-id: 6dd099fd8184985de3aa9f1b0669c178b304748a [formerly f747c973c781afeb8b1d37d023afb49c0044440a]
    Former-commit-id: f180a5a08b51343bb18f097711cfe8eacf555a00
    Former-commit-id: abdf29cb69e883c2daa4cf58098055e6ee0edba2

--- _targets_r/targets/retro_ebola_obs.R
+++ _targets_r/targets/retro_ebola_obs.R
@@ -2,7 +2,8 @@ tar_target(
   retrospective_ebola_obs,
   case_study_data |>
     filter_obs_by_ptime(
-      obs_time = ebola_estimation_times[, "time"][[1]]
+      obs_time = ebola_estimation_times[, "time"][[1]],
+      obs_at = "max_secondary"
     ) |>
     DT(, scenario := ebola_estimation_times[, "scenario"][[1]]) |>
     DT(, obs_type := "retrospective"),
