2493edb091113950a9d480974567c88d2ee3b51d
Sam <s.e.abbott12@gmail.com>
2023-02-07 17:32:15 +0000

    add missing TODOs
    Former-commit-id: cd1a5ce3e4a3bdfd4ffde775ce76149d3559e220
    Former-commit-id: d7f70bd4cee353f5ac84d1be19dd80126ee84f12
    Former-commit-id: 15c2025795ade872cc4db8149651f50ab60210fb [formerly f26c851763611392968cbbfa6bb882b7744e7c1a]
    Former-commit-id: d86c41e4a2a9b108fca297dc970ce64493252002

--- scripts/figures/exponential.R
+++ scripts/figures/exponential.R
@@ -110,6 +110,9 @@ facet_wrap(vars(distribution_stat), ncol = 1)
 # TODO: This currently roles all the replicates into one plot. We might want another version (for the SI) that splits these out so we can explore the amount of variation. # nolint
 
 # TODO: Add coverage plot for each model
+
+# TODO: Make CRPS scoring plot
+
 # Plot posterior densities for each parameter by model and observation type.
 # Filter out outlier values for the sake of plotting
 parameter_density_plot <- e_samples |>
