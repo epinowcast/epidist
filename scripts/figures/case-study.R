7960e3bbe5c444bfbb0c6cf6c2ee1a433954e5fe
Sam <s.e.abbott12@gmail.com>
2023-02-17 13:34:35 +0000

    change axis label

--- scripts/figures/case-study.R
+++ scripts/figures/case-study.R
@@ -9,6 +9,7 @@ library(ggplot2) # for plotting
 library(patchwork) # for combining plots
 library(stringr) # for string manipulation
 library(forcats) # manipulate factors
+
 # Load case study data
 case_study_obs <- fread(here("data/scenarios/ebola_case_study.csv"))
 
@@ -61,6 +62,8 @@ truncated_cs_obs_by_window <- construct_cases_by_obs_window(
 
 obs_plot <- plot_cases_by_obs_window(truncated_cs_obs_by_window)
 
+
+# Calculate forward, retrospective and real-time mean estimates
 case_study_obs_summ_forward <- case_study_obs |>
   group_by(ptime_daily) |>
   summarize(
@@ -102,23 +105,41 @@ case_study_obs_summ_realt <- case_study_obs |>
   mutate(
     ntotal = cumsum(n),
     mean = cumsum(sum1) / ntotal,
-    sd = sqrt((cumsum(sum2) - 2 * cumsum(sum1) * mean + mean^2 * ntotal) / ntotal),
+    sd = sqrt(
+      (cumsum(sum2) - 2 * cumsum(sum1) * mean + mean^2 * ntotal) / ntotal
+    ),
     se = sd / sqrt(ntotal),
     lwr = mean - 1.96 * se,
     upr = mean + 1.96 * se
   )
 
-tv_plot <- ggplot(case_study_obs) +
-  geom_smooth(aes(ptime_daily, delay_daily, col = "Forward", fill = "Forward"), alpha = 0.2) +
-  geom_ribbon(data = case_study_obs_summ_retro, aes(ptime_daily, ymin = lwr, ymax = upr, fill = "Retrospective"), alpha = 0.2) +
-  geom_line(data = case_study_obs_summ_retro, aes(ptime_daily, mean, col = "Retrospective")) +
-  geom_ribbon(data = case_study_obs_summ_realt, aes(stime_daily, ymin = lwr, ymax = upr, fill = "Real-time"), alpha = 0.2) +
-  geom_line(data = case_study_obs_summ_realt, aes(stime_daily, mean, col = "Real-time")) +
+# Plot mean estimates
+tv_plot <- list(
+  case_study_obs |>
+    DT(, `:=`(time = ptime_daily, obs = delay_daily, type = "Forward")),
+  case_study_obs_summ_retro |>
+    DT(, `:=`(time = ptime_daily, type = "Retrospective")),
+  case_study_obs_summ_realt |>
+    DT(, `:=`(time = stime_daily, type = "Real-time"))
+  ) |>
+  rbindlist(fill = TRUE) |>
+  DT(,
+   type := factor(
+    type, levels = c("Real-time", "Retrospective", "Forward"))
+  ) |>
+  ggplot() +
+  aes(x = time, col = type, fill = type) +
+  geom_ribbon(
+    aes(ymin = lwr, ymax = upr), alpha = 0.2
+  ) +
+  geom_line(aes(y = mean)) +
+  geom_smooth(aes(y = obs), alpha = 0.2) +
   geom_vline(xintercept = c(60, 120, 180, 240), lty = 2, alpha = 0.9) +
   scale_x_continuous("Days") +
   scale_y_continuous("Mean delay (days)") +
-  scale_fill_brewer("Estimation method", palette = "Dark2") +
-  scale_color_brewer("Estimation method", palette = "Dark2") +
+  scale_fill_brewer(
+    "Estimation method", palette = "Dark2", aesthetics = c("fill", "colour")
+  ) +
   theme_bw() +
   theme(
     legend.position = "bottom"
@@ -141,7 +162,6 @@ retro_cs_obs <- obs_times |>
 
 combined_cs_obs <- rbind(truncated_cs_obs, retro_cs_obs)
 
-## couldn't figure out how to change the fill... sorry!
 empirical_pmf_plot <- combined_cs_obs |>
   DT(delay_daily <= 20) |>
   DT(obs_at != 483) |>
@@ -154,12 +174,11 @@ empirical_pmf_plot <- combined_cs_obs |>
     aes(
       y = after_stat(density), fill = type
     ),
-    binwidth = 1,
-    position = "dodge", col = "#696767b1", alpha = 0.5
+    binwidth = 1, position = "dodge", alpha = 0.6, col = "#696767b1"
   ) +
-  scale_fill_manual(values = palette.colors(3, "Dark2")[2:3]) +
+  scale_fill_brewer("Estimation method", palette = "Dark2") +
   theme_bw() +
-  theme(legend.position = "none") +
+  theme(legend.position = "bottom") +
   labs(x = "Days", y = "Density") +
   facet_wrap(vars(obs_at), nrow = 1)
 
@@ -185,16 +204,15 @@ case_study_obs_trunc_prop <- merge(
 ) %>%
   arrange(cohort) %>%
   mutate(
-    trunc = n_obs / n_total
+    trunc = 1 - n_obs / n_total
   )
 
 trunc_prop_plot <- ggplot(case_study_obs_trunc_prop) +
   geom_line(aes(cohort, trunc)) +
   geom_vline(xintercept = c(60, 120, 180, 240), lty = 2, alpha = 0.9) +
   scale_x_continuous("Days") +
-  scale_y_continuous("Proportion observation truncated") +
+  scale_y_continuous("Unobserved secondary events", labels = scales::percent) +
   theme_bw()
-
 
 # Plot posterior densities for each parameter by model and observation type.
 # Filter out outlier values for the sake of plotting
@@ -205,36 +223,30 @@ parameter_density_plot <- cs_samples |>
   DT(parameter %in% c("mean", "sd")) |>
   DT(, scenario := gsub(" days", x = scenario, replacement = "") |>
     factor(levels = obs_times) |>
-    fct_rev()) |>
+    fct_rev()
+  ) |>
   DT(, obs_type := str_to_sentence(obs_type)) |>
-  DT(, model := factor(model, levels = models$model))
-
-# Plot posterior densities for each parameter by model and observation type.
-# Filter out outlier values for the sake of plotting
-parameter_density_plot <- clean_cs_samples |>
-  draws_to_long() |>
-  DT(value <= 10) |>
-  DT(value >= -10) |>
-  DT(parameter %in% c("mean", "sd")) |>
   DT(, parameter := str_to_sentence(parameter)) |>
+  DT(, model := factor(model, levels = models$model)) |>
   plot_recovery(y = scenario, fill = obs_type) +
   facet_grid(
     vars(model), vars(parameter),
     labeller = label_wrap_gen(multi_line = TRUE),
     scales = "free_x"
-  ) +
+    ) +
   scale_fill_brewer(palette = "Dark2") +
   guides(
     fill = guide_legend(title = "Estimation method"), col = guide_none()
   ) +
   labs(
-    y = "Observation day", x = "Parameter estimate"
+    y = "Observation day", x = "Days"
   ) +
   theme(legend.position = "bottom")
 
+
 case_study_plot1 <- obs_plot +
   tv_plot +
-  empirical_pmf_plot +
+  (empirical_pmf_plot + guides(fill = guide_none())) +
   trunc_prop_plot +
   plot_annotation(tag_levels = "A") +
   plot_layout(guides = "collect", nrow = 4) &
@@ -249,11 +261,11 @@ case_study_plot2 <- parameter_density_plot +
 # Save combined plots
 ggsave(
   here("figures", "case_study1.png"), case_study_plot1,
-  height = 12, width = 10, dpi = 330
+  height = 16, width = 16, dpi = 330
 )
 
 # Save combined plots
 ggsave(
   here("figures", "case_study2.png"), case_study_plot2,
-  height = 12, width = 10, dpi = 330
+  height = 16, width = 16, dpi = 330
 )
