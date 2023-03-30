ed19695e5dcd102f3135e8e37ecaccdfff640678
Sam <s.e.abbott12@gmail.com>
2023-03-30 18:10:38 +0100

    start implementing retrospective incidence method into pipeline

---
+++ _targets_r/targets/retro_exponential_incidence.R
@@ -0,0 +1,9 @@
+tar_target(retro_exponential_incidence, {
+  simulated_observations_exponential |> 
+    filter_obs_by_ptime(
+        obs_time = 30,
+        obs_at = "max_secondary"
+    ) |>
+    event_to_incidence()  |>
+    DT(, obs_time := 30)
+})
