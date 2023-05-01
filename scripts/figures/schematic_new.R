44d836dd401b64f3c21eb138faf488ffa5d805da
Daniel <parksw3@mcmaster.ca>
2023-05-01 02:26:30 -0400

    a bit more figure tweaking
    Former-commit-id: 7a366dae099726adb3b474508425f3d968073a71 [formerly 01f9cf5055e6bdf11293cf45fec35bb5c5d14e2a]
    Former-commit-id: d98a8a47a76d6fd9ab57bb10d47bf8ffa4a15fdf
    Former-commit-id: 6ab4b0bb19d1aa0f1e9b6336a6a22b9d7d9de914

--- scripts/figures/schematic_new.R
+++ scripts/figures/schematic_new.R
@@ -4,6 +4,8 @@ library(dplyr)
 library(ggplot2)
 library(patchwork)
 
+size <- 16
+
 outbreak <- simulate_exponential_cases(sample_size=10000, seed=123, t=30) |>
   arrange(ptime) |>
   mutate(
@@ -28,9 +30,9 @@ g1 <- ggplot(obs) +
   geom_vline(xintercept=25, lty=2, lwd=1) +
   annotate("text", x=15, y=9800, label="Reference time") +
   ggtitle("A. Empirical density") +
-  scale_x_continuous("Time (days)", expand=c(0, 0)) +
+  scale_x_continuous("Time (days)", expand=c(0, 0), limits=c(0, 60)) +
   scale_y_continuous("id", expand=c(0, 0)) +
-  theme_bw()
+  theme_bw(base_size = size)
 
 obs_forward <- obs |>
   filter(ptime_daily==25)
@@ -46,14 +48,14 @@ forward <- ggplot(obs_forward_sample) +
   ggtitle("B. Forward") +
   scale_x_continuous("Time (days)") +
   scale_y_discrete("id") +
-  theme_bw()
+  theme_bw(base_size = size)
 
 forward_delay <- ggplot(obs_forward) +
   geom_density(aes(delay), fill="orange", alpha=0.3) +
   geom_function(fun=function(x) dlnorm(x, 1.8, 0.5), lwd=1, n=201) +
   scale_x_continuous("Delay (days)", limits=c(0, 35)) +
   scale_y_continuous("Density") +
-  theme_bw()
+  theme_bw(base_size = size)
 
 obs_backward <- obs |>
   filter(stime_daily==25)
@@ -69,14 +71,14 @@ backward <- ggplot(obs_backward_sample) +
   ggtitle("C. Backward") +
   scale_x_continuous("Time (days)") +
   scale_y_discrete("id") +
-  theme_bw()
+  theme_bw(base_size = size)
 
 backward_delay <- ggplot(obs_backward) +
   geom_density(aes(delay), fill="purple", alpha=0.3) +
   geom_function(fun=function(x) dlnorm(x, 1.8, 0.5), lwd=1, n=201) +
   scale_x_continuous("Delay (days)", limits=c(0, 35)) +
   scale_y_continuous("Density") +
-  theme_bw()
+  theme_bw(base_size = size)
 
 obs_truncation <- obs |>
   filter(ptime < 25) 
@@ -98,7 +100,7 @@ truncation <- ggplot(obs_truncation_sample) +
   scale_y_discrete("id") +
   scale_linetype_manual(values=c(3, 1)) +
   scale_fill_manual(values=c("#EEBFF2", "purple")) +
-  theme_bw() +
+  theme_bw(base_size = size) +
   theme(
     legend.position = "none"
   )
@@ -108,7 +110,7 @@ truncation_delay <- ggplot(filter(obs_truncation, stime < 25)) +
   geom_function(fun=function(x) dlnorm(x, 1.8, 0.5), lwd=1, n=201) +
   scale_x_continuous("Ddelay (days)", limits=c(0, 35)) +
   scale_y_continuous("Density") +
-  theme_bw()
+  theme_bw(base_size = size)
 
 censor <- ggplot(obs_truncation_sample) +
   geom_point(aes(ptime_daily, as.factor(case)), size=4, shape=21, fill="orange") +
@@ -118,7 +120,7 @@ censor <- ggplot(obs_truncation_sample) +
   ggtitle("E. Daily censoring") +
   scale_x_continuous("Time (days)", limits=c(0, 35)) +
   scale_y_discrete("id") +
-  theme_bw() +
+  theme_bw(base_size = size) +
   theme(
     legend.position = "none"
   )
@@ -128,13 +130,13 @@ censor_delay <- ggplot(obs_truncation) +
   geom_function(fun=function(x) dlnorm(x, 1.8, 0.5), lwd=1, n=201) +
   scale_x_continuous("Delay (days)", limits=c(0, 35)) +
   scale_y_continuous("Density") +
-  theme_bw()
+  theme_bw(base_size = size)
 
 p <- g1 + (forward + forward_delay +
   backward + backward_delay +
   truncation + truncation_delay +
   censor + censor_delay +
   plot_layout(nrow=2)) +
-  plot_layout(nrow=1, widths=c(1, 2))
+  plot_layout(nrow=1, widths=c(0.6, 2))
 
-ggsave("figures/schematic_new.pdf", p, width = 15, height = 6)
+ggsave("figures/schematic_new.pdf", p, width = 20, height = 8)
