5a939ceee2fd1e813e8d81b1cc4184994e087fc4
Sam <s.e.abbott12@gmail.com>
2023-05-24 15:17:03 +0100

    fiddle with epinowcast to get working with targets
    Former-commit-id: 69d4537d920f2d99e9a9ee9d6054afd5b0e4db0f [formerly db0c82dd04c22cda29fff94620fb43c28315533a]
    Former-commit-id: 9dea84620a37e9fa537184d38ca1192ed6d864c1
    Former-commit-id: 651b2234dfd5fea8d662d9512510685397887cd9

--- _targets_r/targets/save_models.R
+++ _targets_r/targets/save_models.R
@@ -3,5 +3,9 @@ tar_file(
   data.table(
     model = names(models), in_code = machine_model_names
   ) |>
+    rbind(data.table(
+      model = "Joint incidence and delay estimation",
+      in_code = "epinowcast"
+    )) |>
     save_csv("models.csv", path = "data/meta")
 )
