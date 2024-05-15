9ff7788462ab1246c9442c031e09b6f5c14d08f6
athowes <adamthowes@gmail.com>
2024-05-15 14:41:26 +0100

    Add example test data generation in data-raw and as .rda

---
+++ data-raw/create_test_data.R
@@ -0,0 +1,19 @@
+library(data.table)
+
+set.seed(101)
+
+meanlog <- 1.8
+sdlog <- 0.5
+obs_time <- 25
+sample_size <- 200
+
+example_obs <- simulate_gillespie() |>
+  simulate_secondary(
+    meanlog = meanlog,
+    sdlog = sdlog
+  ) |>
+  observe_process() |>
+  filter_obs_by_obs_time(obs_time = obs_time) %>%
+  .[sample(seq_len(.N), sample_size, replace = FALSE)]
+
+usethis::use_data(example_obs, overwrite = TRUE)
