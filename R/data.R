d95a0ab5a23390d902c5af19ba3d06b2d48f8ccd
athowes <adamthowes@gmail.com>
2024-05-15 15:50:52 +0100

    Add data documentation

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#' Example epidist data
+#'
+#' Simulated outbreak data with interval censoring and right truncation.
+#'
+#' @format A `data.table` with 200 rows and 17 columns.
+#' @family data
+#' @source <https://github.com/epinowcast/epidist/tree/main/data-raw/create_test_data.R>
+"example_obs"
