336347507bdd101b09cad472bd5da13830f89116
Adam Howes <adamthowes@gmail.com>
2024-05-16 13:49:42 +0100

    Issue 40: Add example test data (#41)
    * Add example test data generation in data-raw and as .rda
    
    * Make the test data internal
    
    * Delete PNAS data (assume copied here from another package)
    
    * Revert back to non-internal data
    
    * Add data documentation
    
    * Move simulated data generation from data-raw to tests/testthat
    
    * Add PNAS ebola data in raw xlsx format with script for saving it to rda in data
    
    * Remove simulated data from data/
    
    * Clean the ebola column names
    
    * Document ebola data
    
    * Add readxl and janitor to Suggests
    
    * Add newline to end of file
    
    * Add ask to cite and spacing change
    
    * Change name to "ebola_outbreak_sierra_leone" and document roxygen2
    
    * Fix linter issues
    
    * Rename to sierra_leone_ebola_outbreak_data
    
    * Use shorter ebola data name, and change code style
    
    Former-commit-id: d140527a1fb53dac63dfc8fbfcb90c65142d0680
    Former-commit-id: 4ec821ababa950c635bdc96ad43a3dc1def7f70a
    Former-commit-id: 193fd3c60e41e5a4c319d604b4cf71f2cb16a25e [formerly e8b98fe8c15a94f22f06c6a04dd517c3535e1387]
    Former-commit-id: ae71e652bc5e2f008ccf024c951af8e5d91e98a9

---
+++ R/data.R
@@ -0,0 +1,19 @@
+#' Ebola linelist data from Fang et al. (2016)
+#'
+#' Linelist data for the Ebola virus collected in Sierra Leone. If you use this
+#' data in your work, please cite the corresponding paper.
+#'
+#' @format A `tibble` with 8,358 rows and 8 columns:
+#' \describe{
+#'   \item{id}{Unique identification number for the case}
+#'   \item{name}{Name as character, omitted}
+#'   \item{age}{Age as numeric}
+#'   \item{sex}{Sex as character, either "F", "M" or NA}
+#'   \item{date_of_symptom_onset}{The date symptoms began}
+#'   \item{date_of_sample_tested}{The date the sample was tested}
+#'   \item{district}{The district (ADM2)}
+#'   \item{chiefdom}{The chiefdom (ADM3)}
+#' }
+#' @family data
+#' @source <https://www.pnas.org/doi/full/10.1073/pnas.1518587113>
+"sierra_leone_ebola_data"
---
+++ tests/testthat/setup.R
@@ -0,0 +1,15 @@
+set.seed(101)
+
+meanlog <- 1.8
+sdlog <- 0.5
+obs_time <- 25
+sample_size <- 200
+
+sim_obs <- simulate_gillespie() |>
+  simulate_secondary(
+    meanlog = meanlog,
+    sdlog = sdlog
+  ) |>
+  observe_process() |>
+  filter_obs_by_obs_time(obs_time = obs_time) %>%
+  .[sample(seq_len(.N), sample_size, replace = FALSE)]
