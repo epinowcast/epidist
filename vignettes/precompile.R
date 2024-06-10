5f1fea15403b3ff1ab7b58a44527b68f3b025ef6
athowes <xwg3@cdc.gov>
2024-06-10 10:26:30 +0100

    Precomputation method

---
+++ vignettes/precompile.R
@@ -0,0 +1,12 @@
+# Precompiled vignettes with long run times
+library(knitr)
+library(usethis)
+
+wd <- getwd()
+setwd(proj_path("vignettes"))
+markerpat <- "\\.orig$"
+tocompile <- list.files(pattern = markerpat)
+lapply(tocompile, \(x) {
+  knit(x, sub(markerpat, "", x))
+})
+setwd(wd)
