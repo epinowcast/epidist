aab453335de80864a6615fa8666227f6eae0f997
athowes <xwg3@cdc.gov>
2024-06-12 13:24:08 +0100

    Fix lint (did catch bug here!)

--- R/defaults.R
+++ R/defaults.R
@@ -15,53 +15,53 @@ epidist_prepare.default <- function(data, model, ...) {
 }
 
 #' Default method for defining a model specific formula
-#' 
+#'
 #' @inheritParams epidist_formula
 #' @param ... Additional arguments for method.
 #' @family defaults
 #' @export
 epidist_formula.default <- function(data, ...) {
   stop(
-    "No epidist_formula method implemented for the class ", class(x), "\n",
+    "No epidist_formula method implemented for the class ", class(data), "\n",
     "See methods(epidist_formula) for available methods"
   )
 }
 
 #' Default method for defining a model specific family
-#' 
+#'
 #' @inheritParams epidist_family
 #' @param ... Additional arguments for method.
 #' @family defaults
 #' @export
 epidist_family.default <- function(data, ...) {
   stop(
-    "No epidist_family method implemented for the class ", class(x), "\n",
+    "No epidist_family method implemented for the class ", class(data), "\n",
     "See methods(epidist_family) for available methods"
   )
 }
 
 #' Default method for defining model specific priors
-#' 
+#'
 #' @inheritParams epidist_prior
 #' @param ... Additional arguments for method.
 #' @family defaults
 #' @export
 epidist_prior.default <- function(data, ...) {
   stop(
-    "No epidist_prior method implemented for the class ", class(x), "\n",
+    "No epidist_prior method implemented for the class ", class(data), "\n",
     "See methods(epidist_prior) for available methods"
   )
 }
 
 #' Default method for defining model specific Stan code
-#' 
+#'
 #' @inheritParams epidist_stancode
 #' @param ... Additional arguments for method.
 #' @family defaults
 #' @export
 epidist_stancode.default <- function(data, ...) {
   stop(
-    "No epidist_stancode method implemented for the class ", class(x), "\n",
+    "No epidist_stancode method implemented for the class ", class(data), "\n",
     "See methods(epidist_stancode) for available methods"
   )
 }
@@ -83,7 +83,7 @@ epidist.default <- function(data, formula = epidist_formula(data),
     formula = formula, family = family, prior = prior, stanvars = stancode,
     backend = "cmdstanr", data = data, ...
   )
-  
+
   class(fit) <- c(class(fit), "epidist_fit")
   
   return(fit)
