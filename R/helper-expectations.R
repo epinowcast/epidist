fa585e48cf9e637073e65b0f63aff03c5b20eb7f
athowes <xwg3@cdc.gov>
2024-06-17 19:27:18 +0100

    Add start to integration tests from other PR

---
+++ R/helper-expectations.R
@@ -0,0 +1,10 @@
+expect_convergence <- function(fit, per_dts = 0.05, treedepth = 10,
+                               rhat = 1.05) {
+  
+  max_treedepth <- rstan::get_num_max_treedepth(fit$fit)
+  max_rhat <- max(brms::rhat(fit))
+  
+  #testthat::expect_lt(per_divergent_transitions, per_dts)
+  testthat::expect_lt(max_treedepth, treedepth)
+  testthat::expect_lt(max_rhat, rhat)
+}
