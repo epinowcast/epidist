ebfe198b91f10d5f1c965e97f6d71694800d4317
athowes <xwg3@cdc.gov>
2024-06-17 10:56:17 +0100

    Rename to static_code_analysis and delete saved .png

--- inst/static_code_analysis.R
+++ inst/static_code_analysis.R
@@ -4,6 +4,4 @@ gg <- CodeDepends::makeCallGraph("package:epidist")
 gg <- layoutGraph(gg, layoutType = "neato")
 graph.par(list(nodes = list(fontsize = 40)))
 
-png(filename = "inst/code_depends.png", width = 5, height = 5, units = "in", res = 300)
 renderGraph(gg)
-dev.off()
