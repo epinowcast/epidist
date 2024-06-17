a4b4a1bd75e145fbfdf73d9bef253e874aef07b2
Adam Howes <xwg3@cdc.gov>
2024-06-17 12:36:37 +0100

    Issue 71: Add help for passing priors (#95)
    * Rebase
    
    * Priors testing -- to be moved to tests
    
    * Clear out old attempt and describe proposed workflow
    
    * Remove scratch code
    
    * Fix rebase mess! Need to get better at these :(
    
    * Another rebase mistake
    
    * If I remove this will it be passed in via ...?
    
    * Rename epidist_priors to epidist_prior to be more inline with brms prior argument
    
    * Propose new default priors
    
    * Document default priors
    
    * Run document
    
    * Missed this one!
    
    * Document
    
    * Add arguments to epidist_prior.epidist_latent_individual
    
    * Fix lint (did catch bug here!)
    
    * Fix linter issues
    
    * More linting
    
    * Alter the prior on sdlog as it's on the logarithm again
    
    * Move prior to N(0, 0.5) rather than N(0.5, 0.5) (forgot to actually do this in the code)
    
    * Use prior_summary
    
    * Fix lint and document
    
    * Update priors description
    
    * Fix lint

--- R/generics.R
+++ R/generics.R
@@ -32,11 +32,11 @@ epidist_family <- function(data, ...) {
 #'
 #' @inheritParams epidist_prepare
 #' @param ... Additional arguments for method.
-#' @rdname epidist_priors
+#' @rdname epidist_prior
 #' @family generics
 #' @export
-epidist_priors <- function(data, ...) {
-  UseMethod("epidist_priors")
+epidist_prior <- function(data, ...) {
+  UseMethod("epidist_prior")
 }
 
 #' Define model specific Stan code
@@ -54,7 +54,7 @@ epidist_stancode <- function(data, ...) {
 #'
 #' @param formula A formula as defined using [epidist_formula()]
 #' @param family ...
-#' @param priors ...
+#' @param prior ...
 #' @param stancode ...
 #' @param fn Likely `brms::brm`. Also possible to be `brms::make_stancode` or
 #' `brms::make_standata`.
@@ -62,6 +62,6 @@ epidist_stancode <- function(data, ...) {
 #' @param ... Additional arguments for method.
 #' @family generics
 #' @export
-epidist <- function(data, formula, family, priors, stancode, fn, ...) {
+epidist <- function(data, formula, family, prior, stancode, fn, ...) {
   UseMethod("epidist")
 }
