2d563372d460edea4a4f17fd04f2231d68386d45
athowes <xwg3@cdc.gov>
2024-06-17 11:59:24 +0100

    First set of tests in. Includes convergence helper. To review in meeting

--- tests/testthat/test-latent_individual.R
+++ tests/testthat/test-latent_individual.R
@@ -1,49 +1,60 @@
 # Assumes that tests/testthat/setup.R has been run
 
+# Generate observation data in correct format for the latent_individual model
 prep_obs <- epidist_prepare(sim_obs, model = "latent_individual")
 
-test_that("epidist_formula.epidist_latent_individual works with default parameters", {
-  formula <- epidist_formula(prep_obs)
-  expect_s3_class(formula, "brmsformula")
+test_that("epidist_formula.epidist_latent_individual with default settings produces a brmsformula with the correct intercept only formula", { # nolint: line_length_linter.
+  form <- epidist_formula(prep_obs)
+  expect_s3_class(form, "brmsformula")
+  expect_equal(
+    deparse(form$formula),
+    "delay_central | vreal(obs_t, pwindow_upr, swindow_upr) ~ 1"
+  )
+  expect_equal(
+    deparse(form$pforms$sigma),
+    "sigma ~ 1"
+  )
 })
 
-family <- epidist_family(prep_obs)
-# TODO: add tests for formula
-family
+test_that("epidist_family.epidist_latent_individual with default settings produces an object of the right class", { # nolint: line_length_linter.
+  family <- epidist_family(prep_obs)
+  expect_s3_class(family, "customfamily")
+  expect_s3_class(family, "brmsfamily")
+  expect_s3_class(family, "family")
+})
 
 priors <- epidist_priors(prep_obs)
-# TODO: add tests for formula
+# TODO: add tests for priors: waiting on prior PR being merged for this
 priors
 
-stancode <- epidist_stancode(prep_obs)
-# TODO: add tests for stancode
-stancode
+test_that("epidist_stancode.epidist_latent_individual", { # nolint: line_length_linter.
+  stancode <- epidist_stancode(prep_obs)
+  expect_equal(1, 1)
+})
 
-fit <- epidist(
-  data = prep_obs,
-  formula = formula,
-  family = family,
-  priors = priors,
-  stancode = stancode
-)
-# TODO: add tests for epidist
-# Note this is the same thing as fit2 <- epidist(prep_obs)
+test_that("epidist.epidist_latent_individual fits in the default case", {
+  fit <- epidist(data = prep_obs)
+  expect_s3_class(fit, "brmsfit")
+  expect_s3_class(fit, "epidist_fit")
+  expect_convergence(fit)
+})
 
-# Could also do tests on the stancode
-stancode <- epidist(prep_obs, fn = brms::make_stancode)
-stancode
+# Could also do tests on the stancode?
+stancode_external <- epidist(prep_obs, fn = brms::make_stancode)
 
-# Could also test other distribution functionality
-fit_gamma <- epidist(
-  data = prep_obs,
-  formula = formula,
-  family = epidist_family(prep_obs, family = "gamma"),
-  priors = priors,
-  stancode = epidist_stancode(
-    prep_obs,
-    family = epidist_family(prep_obs, family = "gamma")
+test_that("epidist.epidist_latent_individual fits for a gamma delay distribution", { # nolint: line_length_linter.
+  fit_gamma <- epidist(
+    data = prep_obs,
+    family = epidist_family(prep_obs, family = "gamma"),
+    stancode = epidist_stancode(
+      prep_obs,
+      family = epidist_family(prep_obs, family = "gamma")
+    )
   )
-)
+  expect_s3_class(fit, "brmsfit")
+  expect_s3_class(fit, "epidist_fit")
+  expect_convergence(fit)
+})
 
 # Could also test how default methods work
 x <- list()
