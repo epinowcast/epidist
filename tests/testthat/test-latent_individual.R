4e5b01fc973cbdceac8582ffcd510cb50b44663d
athowes <xwg3@cdc.gov>
2024-06-17 19:21:17 +0100

    Remove integration tests

--- tests/testthat/test-latent_individual.R
+++ tests/testthat/test-latent_individual.R
@@ -32,30 +32,6 @@ test_that("epidist_stancode.epidist_latent_individual", { # nolint: line_length_
   expect_equal(1, 1)
 })
 
-test_that("epidist.epidist_latent_individual fits in the default case", {
-  fit <- epidist(data = prep_obs)
-  expect_s3_class(fit, "brmsfit")
-  expect_s3_class(fit, "epidist_fit")
-  expect_convergence(fit)
-})
-
-# Could also do tests on the stancode?
-stancode_external <- epidist(prep_obs, fn = brms::make_stancode)
-
-test_that("epidist.epidist_latent_individual fits for a gamma delay distribution", { # nolint: line_length_linter.
-  fit_gamma <- epidist(
-    data = prep_obs,
-    family = epidist_family(prep_obs, family = "gamma"),
-    stancode = epidist_stancode(
-      prep_obs,
-      family = epidist_family(prep_obs, family = "gamma")
-    )
-  )
-  expect_s3_class(fit_gamma, "brmsfit")
-  expect_s3_class(fit_gamma, "epidist_fit")
-  expect_convergence(fit_gamma)
-})
-
 # Could also test how default methods work
 x <- list()
 epidist_prepare(x)
