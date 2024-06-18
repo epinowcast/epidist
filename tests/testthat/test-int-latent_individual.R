9f6a77d4978b896ac4f8e468fb9f84ea99a5d988
athowes <xwg3@cdc.gov>
2024-06-18 14:18:49 +0100

    Add test of compilation for Stan code

--- tests/testthat/test-int-latent_individual.R
+++ tests/testthat/test-int-latent_individual.R
@@ -1,7 +1,9 @@
-# Assumes that tests/testthat/setup.R has been run
-
-# Generate observation data in correct format for the latent_individual model
 prep_obs <- epidist_prepare(sim_obs, model = "latent_individual")
+
+test_that("epidist.epidist_latent_individual can be used to extract valid Stan code which compiles", { # nolint: line_length_linter.
+  stancode <- epidist(data = prep_obs, fn = brms::make_stancode)
+  compiled_model <- rstan::stan_model(model_code = stancode)
+})
 
 test_that("epidist.epidist_latent_individual fits in the default case", {
   fit <- epidist(data = prep_obs)
@@ -9,9 +11,6 @@ test_that("epidist.epidist_latent_individual fits in the default case", {
   expect_s3_class(fit, "epidist_fit")
   expect_convergence(fit)
 })
-
-# Could also do tests on the stancode?
-stancode_external <- epidist(prep_obs, fn = brms::make_stancode)
 
 test_that("epidist.epidist_latent_individual fits for a gamma delay distribution", { # nolint: line_length_linter.
   fit_gamma <- epidist(
