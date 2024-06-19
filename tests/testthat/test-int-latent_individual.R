9dc05e25cbb727c0d761cf1ba34c10c0b2322fb7
athowes <xwg3@cdc.gov>
2024-06-19 14:48:08 +0100

    Compliation and convergence tests for gamma and 1 + sex model

--- tests/testthat/test-int-latent_individual.R
+++ tests/testthat/test-int-latent_individual.R
@@ -1,8 +1,8 @@
 prep_obs <- epidist_prepare(sim_obs, model = "latent_individual")
 
-test_that("epidist.epidist_latent_individual can be used to extract valid Stan code which compiles", { # nolint: line_length_linter.
+test_that("epidist.epidist_latent_individual Stan code compiles in the default case", { # nolint: line_length_linter.
   stancode <- epidist(data = prep_obs, fn = brms::make_stancode)
-  compiled_model <- rstan::stan_model(model_code = stancode)
+  expect_no_error(rstan::stan_model(model_code = stancode))
 })
 
 test_that("epidist.epidist_latent_individual fits in the default case", {
@@ -12,7 +12,21 @@ test_that("epidist.epidist_latent_individual fits in the default case", {
   expect_convergence(fit)
 })
 
-# epidist compiles for gamma delay distribution
+# After merging another PR this code can be simplified
+# (perhaps rebase onto this if merged first)
+
+test_that("epidist.epidist_latent_individual Stan code compiles in the gamma delay case", { # nolint: line_length_linter.
+  stancode_gamma <- epidist(
+    data = prep_obs,
+    family = epidist_family(prep_obs, family = "gamma"),
+    stancode = epidist_stancode(
+      prep_obs,
+      family = epidist_family(prep_obs, family = "gamma")
+    ),
+    fn = brms::make_stancode
+  )
+  expect_no_error(rstan::stan_model(model_code = stancode_gamma))
+})
 
 test_that("epidist.epidist_latent_individual fits for a gamma delay distribution", { # nolint: line_length_linter.
   fit_gamma <- epidist(
@@ -28,5 +42,21 @@ test_that("epidist.epidist_latent_individual fits for a gamma delay distribution
   expect_convergence(fit_gamma)
 })
 
-# epidist compiles for an alternative formula e.g. with 1 + sex
-# epidist fits for an alternative formula e.g. with 1 + sex
+test_that("epidist.epidist_latent_individual Stan code compiles for an alternative formula", { # nolint: line_length_linter.
+  prep_obs$sex <- rbinom(n = nrow(prep_obs), size = 1, prob = 0.5)
+  formula_sex <- epidist_formula(prep_obs, delay_central = ~ 1 + sex,
+                                 sigma = ~ 1 + sex)
+  stancode_sex <- epidist(data = prep_obs, formula = formula_sex,
+                          fn = brms::make_stancode)
+  expect_no_error(rstan::stan_model(model_code = stancode_sex))
+})
+
+test_that("epidist.epidist_latent_individual fits for an alternative formula", {
+  prep_obs$sex <- rbinom(n = nrow(prep_obs), size = 1, prob = 0.5)
+  formula_sex <- epidist_formula(prep_obs, delay_central = ~ 1 + sex,
+                                 sigma = ~ 1 + sex)
+  fit_sex <- epidist(data = prep_obs, formula = formula_sex)
+  expect_s3_class(fit_sex, "brmsfit")
+  expect_s3_class(fit_sex, "epidist_fit")
+  expect_convergence(fit_sex)
+})
