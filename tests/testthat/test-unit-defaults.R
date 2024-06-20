23844357aeea2bb027875f32c858cbb7f24657b3
Adam Howes <xwg3@cdc.gov>
2024-06-20 12:01:13 +0100

    Issue 106: Unit testing for `latent_individual` (#107)
    * Remove need for %>%
    
    * Move S3 to outline of tests needed in testthat folder
    
    * First test (S3 class)
    
    * First set of tests in. Includes convergence helper. To review in meeting
    
    * Correction to previous commit (fit_gamma, not fit)
    
    * Remove integration tests
    
    * Remove test-multiply
    
    * Rename to include unit
    
    * Add as_string_formula function (where should this go eventually?)
    
    * Two ideas for formula unit tests
    
    * Add prior test and move default tests into their own script
    
    * Two tests for epidist_family
    
    * Add tests about 1 + sex (should be correct) and 1 + age (should complain that there is no age in the data)
    
    * Add a test for prep_obs classes
    
    * Should throw errors if wrong formula
    
    * Alter default unit tests to pass
    
    * Comment tests which require new features
    
    * Typo here
    
    * Remove this one for now
    
    * Don't need commented code, and lint whitespace
    
    Former-commit-id: 2823fd0f88b73eee01e1f6def83afe06f4c8212e
    Former-commit-id: 8856536a8a8618bbc837a1cabe894957add94e70
    Former-commit-id: 43e3e88ecaaac3ee6c343e00b61fa14f4fd618ac [formerly d278552d8a6a805e5e88ba5b08ddd39e14bd4c27]
    Former-commit-id: 198229e394d0ac3eff02678c8aa45fb4b8df9093

---
+++ tests/testthat/test-unit-defaults.R
@@ -0,0 +1,19 @@
+test_that("epidist_prepare.default gives an error when passed a model which is not implemented", { # nolint: line_length_linter.
+  expect_error(epidist_prepare(sim_obs, model = "not_a_real_model"))
+})
+
+test_that("epidist_family.default gives an error when passed a list", { # nolint: line_length_linter.
+  expect_error(epidist_family(list()))
+})
+
+test_that("epidist_formula.default gives an error when passed a list", { # nolint: line_length_linter.
+  expect_error(epidist_formula(list()))
+})
+
+test_that("epidist_prior.default gives an error when passed a list", { # nolint: line_length_linter.
+  expect_error(epidist_prior(list()))
+})
+
+test_that("epidist_standata.default gives an error when passed a list", { # nolint: line_length_linter.
+  expect_error(epidist_standata(list()))
+})
