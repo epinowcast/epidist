ebcfb476e2f54f829d568329eac95e0193e09a39
Adam Howes <xwg3@cdc.gov>
2024-06-20 12:01:13 +0100

    Issue 106: Unit testing for `latent_individual` (#107)
    * Remove need for %>%
    
    * Move S3 to outline of tests needed in testthat folder
    
    * First test (S3 class)
    
    * First set of tests in. Includes convergence helper. To review in meeting
    
    * Correction to previous commit (fit_gamma, not fit)
    
    * Remove integration tests
    
    * Remove test-multiply
    
    * Rename to include unit
    
    * Add as_string_formula function (where should this go eventually?)
    
    * Two ideas for formula unit tests
    
    * Add prior test and move default tests into their own script
    
    * Two tests for epidist_family
    
    * Add tests about 1 + sex (should be correct) and 1 + age (should complain that there is no age in the data)
    
    * Add a test for prep_obs classes
    
    * Should throw errors if wrong formula
    
    * Alter default unit tests to pass
    
    * Comment tests which require new features
    
    * Typo here
    
    * Remove this one for now
    
    * Don't need commented code, and lint whitespace
    
    Former-commit-id: 644aeea77be2728abf3be758726236808ef59ed9 [formerly 13a32c2f11629f4335c2816003427e306dc02ecc]
    Former-commit-id: 99a33c264fa4a376f2128b1a9e439b784f2d3e79

---
+++ tests/testthat/test-unit-latent_individual.R
@@ -0,0 +1,60 @@
+as_string_formula <- function(formula) {
+  form <- paste(deparse(formula), collapse = " ")
+  form <- gsub("\\s+", " ", form, perl = FALSE)
+  return(form)
+}
+
+# Generate observation data in correct format for the latent_individual model
+prep_obs <- epidist_prepare(sim_obs, model = "latent_individual")
+
+test_that("epidist_prepare.epidist_latent_individual with default settings an object with the correct classes", { # nolint: line_length_linter.
+  expect_s3_class(prep_obs, "data.table")
+  expect_s3_class(prep_obs, "data.frame")
+  expect_s3_class(prep_obs, "epidist_latent_individual")
+})
+
+test_that("epidist_formula.epidist_latent_individual with default settings produces a brmsformula with the correct intercept only formula", { # nolint: line_length_linter.
+  form <- epidist_formula(prep_obs)
+  expect_s3_class(form, "brmsformula")
+  expect_equal(
+    as_string_formula(form$formula),
+    "delay_central | vreal(obs_t, pwindow_upr, swindow_upr) ~ 1"
+  )
+  expect_equal(
+    as_string_formula(form$pforms$sigma),
+    "sigma ~ 1"
+  )
+})
+
+test_that("epidist_formula.epidist_latent_individual with custom formulas produces a brmsformula with correct custom formulas", { # nolint: line_length_linter.
+  prep_obs$sex <- rbinom(n = nrow(prep_obs), size = 1, prob = 0.5)
+  form_sex <- epidist_formula(prep_obs, delay_central = ~ 1 + sex,
+                              sigma = ~ 1 + sex)
+  expect_s3_class(form_sex, "brmsformula")
+  expect_equal(
+    as_string_formula(form_sex$formula),
+    "delay_central | vreal(obs_t, pwindow_upr, swindow_upr) ~ 1 + sex"
+  )
+  expect_equal(
+    as_string_formula(form_sex$pforms$sigma),
+    "sigma ~ 1 + sex"
+  )
+})
+
+test_that("epidist_family.epidist_latent_individual with default settings produces an object of the right class", { # nolint: line_length_linter.
+  family <- epidist_family(prep_obs)
+  expect_s3_class(family, "customfamily")
+  expect_s3_class(family, "brmsfamily")
+  expect_s3_class(family, "family")
+})
+
+test_that("the family argument in epidist_family.epidist_latent_individual passes as expected", { # nolint: line_length_linter.
+  family_gamma <- epidist_family(prep_obs, family = "gamma")
+  expect_equal(family_gamma$name, "latent_gamma")
+})
+
+test_that("epidist_prior.epidist_latent_individual with default settings produces an object of the right class", { # nolint: line_length_linter.
+  prior <- epidist_prior(prep_obs)
+  expect_s3_class(prior, "brmsprior")
+  expect_s3_class(prior, "data.frame")
+})
