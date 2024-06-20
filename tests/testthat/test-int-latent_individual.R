7bc06a668a1d5554d01416ba50e21bdbd6edf69a
athowes <xwg3@cdc.gov>
2024-06-20 14:22:52 +0100

    Use cmdstanr version of compile check rather than rstan

--- tests/testthat/test-int-latent_individual.R
+++ tests/testthat/test-int-latent_individual.R
@@ -2,7 +2,8 @@ prep_obs <- epidist_prepare(sim_obs, model = "latent_individual")
 
 test_that("epidist.epidist_latent_individual Stan code compiles in the default case", { # nolint: line_length_linter.
   stancode <- epidist(data = prep_obs, fn = brms::make_stancode)
-  expect_no_error(rstan::stan_model(model_code = stancode))
+  mod <- cmdstan_model(stan_file = write_stan_file(stancode), compile = FALSE)
+  expect_no_error(mod$compile())
 })
 
 test_that("epidist.epidist_latent_individual fits and the MCMC converges in the default case", { # nolint: line_length_linter.
@@ -25,7 +26,10 @@ test_that("epidist.epidist_latent_individual Stan code compiles in the gamma del
     ),
     fn = brms::make_stancode
   )
-  expect_no_error(rstan::stan_model(model_code = stancode_gamma))
+  mod_gamma <- cmdstan_model(
+    stan_file = write_stan_file(stancode_gamma), compile = FALSE
+  )
+  expect_no_error(mod_gamma$compile())
 })
 
 test_that("epidist.epidist_latent_individual fits and the MCMC converges for a gamma delay distribution", { # nolint: line_length_linter.
@@ -48,7 +52,10 @@ test_that("epidist.epidist_latent_individual Stan code compiles for an alternati
                                  sigma = ~ 1 + sex)
   stancode_sex <- epidist(data = prep_obs, formula = formula_sex,
                           fn = brms::make_stancode)
-  expect_no_error(rstan::stan_model(model_code = stancode_sex))
+  mod_sex <- cmdstan_model(
+    stan_file = write_stan_file(stancode_sex), compile = FALSE
+  )
+  expect_no_error(mod_sex$compile())
 })
 
 test_that("epidist.epidist_latent_individual fits and the MCMC converges for an alternative formula", { # nolint: line_length_linter.
