68ad709a23be3146fc2abbb4b5b76e8fabd34d76
Adam Howes <xwg3@cdc.gov>
2024-06-27 18:00:43 +0100

    Issue 113: Integration testing for `latent_individual` (#114)
    * Add start to integration tests from other PR
    
    * Add test of compilation for Stan code
    
    * Three other possible tests
    
    * Move to correct place
    
    * Compliation and convergence tests for gamma and 1 + sex model
    
    * Add dt to expect_convergence
    
    * Better description of tests (including convergence)
    
    * Use cmdstanr version of compile check rather than rstan
    
    * Update gamma after change of family option PR merge (all passing locally)
    
    * Add gamma data such that the gamma tests are on gamma data
    
    * Add test that model is sampling from prior according to KS statistic
    
    * Increase sample size to help parameter recovery
    
    * Move parameter definitions shared across rlnorm and rgamma
    
    * More verbose test description
    
    * Add test of parameter recovery for lognormal
    
    * No sex effect recovery!
    
    * Wokring on the gamma fit recovery
    
    * Fix lint
    
    * b_sex and b_sigma_sex should be near zero (increase tolerance)
    
    * Remove gamma recovery test
    
    * Remove gamma convergence test
    
    * Add rstan to Suggests as I use it in tests
    
    * Skip tests needing cmdstanr on CRAN
    
    Former-commit-id: fa1be26ccde7763930aeef4be442886d7c9622e4
    Former-commit-id: ff2c350503332c30701f7a9fcee3cf04ed8d7d76
    Former-commit-id: d29d972965b545a27d506542fa5685d7ed0cd785 [formerly 2c9d0bd4186e089956a16fe7a36f995fae83a03c]
    Former-commit-id: 6a2cbc6beb5d9d596e5547e4d4c321f189bd3401

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,12 +1,16 @@
 set.seed(101)
 
+obs_time <- 25
+sample_size <- 500
+
 meanlog <- 1.8
 sdlog <- 0.5
-obs_time <- 25
-sample_size <- 200
+
+# The mean of the lognormal distribution is: exp(meanlog + 0.5 * sdlog^2)
 
 sim_obs <- simulate_gillespie() |>
   simulate_secondary(
+    dist = rlnorm,
     meanlog = meanlog,
     sdlog = sdlog
   ) |>
@@ -14,3 +18,22 @@ sim_obs <- simulate_gillespie() |>
   filter_obs_by_obs_time(obs_time = obs_time)
 
 sim_obs <- sim_obs[sample(seq_len(.N), sample_size, replace = FALSE)]
+
+set.seed(101)
+
+shape <- 2
+rate <- 3
+
+# The mean of the gamma distribution is: shape * rate
+
+sim_obs_gamma <- simulate_gillespie() |>
+  simulate_secondary(
+    dist = rgamma,
+    shape = shape,
+    rate = rate
+  ) |>
+  observe_process() |>
+  filter_obs_by_obs_time(obs_time = obs_time)
+
+sim_obs_gamma <-
+  sim_obs_gamma[sample(seq_len(.N), sample_size, replace = FALSE)]
