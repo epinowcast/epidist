21d393604b5144ca53c9960be4e3c4b460df6033
athowes <xwg3@cdc.gov>
2024-07-08 11:55:43 +0100

    Remove requirement for rstan in tests (use brms)

--- tests/testthat/helper-expectations.R
+++ tests/testthat/helper-expectations.R
@@ -1,7 +1,8 @@
 expect_convergence <- function(fit, per_dts = 0.05, treedepth = 10,
                                rhat = 1.05) {
-  per_divergent_transitions <- mean(rstan::get_divergent_iterations(fit$fit))
-  max_treedepth <- rstan::get_num_max_treedepth(fit$fit)
+  div <- subset(brms::nuts_params(fit), Parameter == "divergent__")
+  per_divergent_transitions <- mean(div$Value)
+  max_treedepth <- brms::control_params(fit)$max_treedepth
   max_rhat <- max(brms::rhat(fit))
   testthat::expect_lt(per_divergent_transitions, per_dts)
   testthat::expect_lt(max_treedepth, treedepth)
