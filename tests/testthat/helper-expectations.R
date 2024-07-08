d60b38b6440437b138a6a7c218126538d42dd6ee
athowes <xwg3@cdc.gov>
2024-07-08 12:01:37 +0100

    Avoid global bindings warning by not using subset

--- tests/testthat/helper-expectations.R
+++ tests/testthat/helper-expectations.R
@@ -1,7 +1,8 @@
 expect_convergence <- function(fit, per_dts = 0.05, treedepth = 10,
                                rhat = 1.05) {
-  div <- subset(brms::nuts_params(fit), Parameter == "divergent__")
-  per_divergent_transitions <- mean(div$Value)
+  np <- brms::nuts_params(fit)
+  indices <- np$Parameter == "divergent__"
+  per_divergent_transitions <- mean(np[indices, ]$Value)
   max_treedepth <- brms::control_params(fit)$max_treedepth
   max_rhat <- max(brms::rhat(fit))
   testthat::expect_lt(per_divergent_transitions, per_dts)
