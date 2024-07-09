3be027edb0172bd888f271d3c067456ef5d079aa
Adam Howes <xwg3@cdc.gov>
2024-07-09 10:05:44 +0100

    Issue 134: Drop rstan dependency (#137)
    * Remove requirement for rstan in tests (use brms)
    
    * Avoid global bindings warning by not using subset
    
    * Extract NUTS parameters manually
    
    * A way to do it with just nuts_params
    
    Former-commit-id: aa184fe3868de0699a9be57f4b9bea1456f16b4d
    Former-commit-id: 56e8db8e487ab143f106889a0dfa9467e45a151e
    Former-commit-id: 9cad4a9762698f6314e32ada224063eb1a4c9a41 [formerly 898854b2c6a61acac8aa92d1b9455ade742b0d50]
    Former-commit-id: 2ae3c7693838668456388ef9c7b93eadc7f18cc2

--- tests/testthat/helper-expectations.R
+++ tests/testthat/helper-expectations.R
@@ -1,7 +1,10 @@
 expect_convergence <- function(fit, per_dts = 0.05, treedepth = 10,
                                rhat = 1.05) {
-  per_divergent_transitions <- mean(rstan::get_divergent_iterations(fit$fit))
-  max_treedepth <- rstan::get_num_max_treedepth(fit$fit)
+  np <- brms::nuts_params(fit)
+  divergent_indices <- np$Parameter == "divergent__"
+  per_divergent_transitions <- mean(np[divergent_indices, ]$Value)
+  treedepth_indices <- np$Parameter == "treedepth__"
+  max_treedepth <- max(np[treedepth_indices, ]$Value)
   max_rhat <- max(brms::rhat(fit))
   testthat::expect_lt(per_divergent_transitions, per_dts)
   testthat::expect_lt(max_treedepth, treedepth)
