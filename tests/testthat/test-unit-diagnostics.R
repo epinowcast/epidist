2a0ff0309214a1d0b866161e4f510c516c14b938
Adam Howes <xwg3@cdc.gov>
2024-07-19 11:24:01 +0100

    Issue 163: Create `epidist_diagnostics` (#175)
    * Remove sample_model
    
    * Start to write epidist_diagnostics
    
    * Rebase merged PRs
    
    * Don't need this to be S3
    
    * Finish removal of S3 and improve tests
    
    * Replace custom code with diagnostics function
    
    * Lint and documentation fixes
    
    * Add documentation to epidist_diagnostics
    
    * Lint and add diagnostics as section
    
    * Add more unit tests for diagnostics
    
    * expect_numeric doesn't exist
    
    * Make tests softer
    
    Former-commit-id: b8ca220f6c7c53a209f818c731d29b40b25c2298 [formerly e1e900b581ace52c1d7da304774f1623dc1c0a59]
    Former-commit-id: 2e5b3af9e2256b55fb2785436eaf15b28bcb5790

---
+++ tests/testthat/test-unit-diagnostics.R
@@ -0,0 +1,31 @@
+test_that("epidist_diagnostics", { # nolint: line_length_linter.
+  skip_on_cran()
+  set.seed(1)
+  prep_obs <- as_latent_individual(sim_obs)
+  fit <- epidist(data = prep_obs, seed = 1)
+  diag <- epidist_diagnostics(fit)
+  expected_names <- c(
+    "time", "samples", "max_rhat", "divergent_transitions",
+    "per_divergent_transitions", "max_treedepth", "no_at_max_treedepth",
+    "per_at_max_treedepth"
+  )
+  expect_equal(names(diag), expected_names)
+  expect_gt(diag$time, 0)
+  expect_gt(diag$samples, 0)
+  expect_gt(diag$max_rhat, 0.9)
+  expect_lt(diag$max_rhat, 1.1)
+  expect_gte(diag$divergent_transitions, 0)
+  expect_lt(diag$divergent_transitions, diag$samples)
+  expect_lt(diag$max_treedepth, 12)
+  expect_lte(diag$no_at_max_treedepth, diag$samples)
+  expect_lte(diag$per_at_max_treedepth, 1)
+  expect_gt(diag$per_at_max_treedepth, 0)
+})
+
+test_that("epidist_diagnostics gives an error when passed model fit using the Laplace algorithm", { # nolint: line_length_linter.
+  skip_on_cran()
+  set.seed(1)
+  prep_obs <- as_latent_individual(sim_obs)
+  fit_laplace <- epidist(data = prep_obs, seed = 1, algorithm = "laplace")
+  expect_error(epidist_diagnostics(fit_laplace))
+})
