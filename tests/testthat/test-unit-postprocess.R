0061f5cfe2cd940450b98999be4e2ba415fede57
athowes <xwg3@cdc.gov>
2024-07-22 09:59:03 +0100

    Finding a barrier here about using brms::predict

--- tests/testthat/test-unit-postprocess.R
+++ tests/testthat/test-unit-postprocess.R
@@ -1,7 +1,20 @@
+set.seed(1)
 prep_obs <- as_latent_individual(sim_obs)
 fit <- epidist(data = prep_obs, seed = 1)
-fit$fit
 
-# Names for the link functions needed
+# Need to modify this (i.e. "latent_lognormal")
+fit$family
+
+# (Which includes these names for the link functions needed)
 fit$family$link
 fit$family$link_sigma
+
+# To work with these existing brms prediction functions
+brms::posterior_epred(fit)
+brms::posterior_predict(fit)
+
+fit$family$family <- "lognormal"
+fit$family$name <- "lognormal"
+
+str(fit$family)
+brms::posterior_predict(fit)
