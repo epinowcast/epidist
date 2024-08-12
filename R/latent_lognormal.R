b1f15360b499258d9a814eec238153365ba7c78a
athowes <xwg3@cdc.gov>
2024-08-12 11:30:11 +0100

    Crawling towards prediction functionality

--- R/latent_lognormal.R
+++ R/latent_lognormal.R
@@ -1,21 +1,43 @@
+#' Draws from the posterior predictive distribution
+#' 
+#' See [`brms::posterior_predict`].
+#'
+#' @param i The index of the observation to predict
+#' @param prep The result of a call to [`brms::posterior_predict`]
+#' @family postprocess
+#' @autoglobal
+#' @export
 posterior_predict_latent_lognormal <- function(i, prep, ...) {
   mu <- brms::get_dpar(prep, "mu", i = i)
   sigma <- brms::get_dpar(prep, "sigma", i = i)
-  vreal1 <- prep$data$vreal1[i]
-  vreal2 <- prep$data$vreal2[i]
-  vreal3 <- prep$data$vreal3[i]
+
   wN <- prep$data$wN[i]
   woverlap <- prep$data$woverlap[i]
   noverlap <- prep$data$noverlap[i]
   
-  swindow_raw <- runif(prep$ndraws, 0, 1)
-  pwindow_raw <- runif(prep$ndraws, 0, 1)
+  # This assumes a certain prior on swindow_raw and pwindow_raw
+  # It would be better to extract these from prep in some way?
+  # Is it also the case that other outputs are correlated to these somehow such
+  # that independent draws are wrong in some way?
+  # Should n be prep$ndraws or 1?
+  swindow_raw <- runif(1, 0, 1)
+  pwindow_raw <- runif(1, 0, 1)
   
+  vreal3 <- prep$data$vreal3[i]
   swindow <- vreal3 * swindow_raw
-  # pwindow <- ...
-  # obs_t <- ...
   
-  y <- prep$data$Y[i]
+  vreal2 <- prep$data$vreal2[i]
+  pwindow <- vreal2[noverlap] * pwindow_raw[noverlap]
+  if (wN) {
+    pwindow[woverlap] <- swindow[woverlap] * pwindow_raw[woverlap]
+  }
+  
+  vreal1 <- prep$data$vreal1[i]
+  obs_t <- vreal1
+  
+  obs_time <- obs_t - pwindow
+  
+  EnvStats::rlnormTrunc(1, meanlog = mu, sdlog = sigma, min = 0, max = Inf)
 }
 
 #' Draws from the expected value of the posterior predictive distribution
@@ -30,4 +52,18 @@ posterior_epred_latent_lognormal <- function(prep) {
   mu <- brms::get_dpar(prep, "mu")
   sigma <- brms::get_dpar(prep, "sigma")
   exp(mu + sigma^2 / 2)
+}
+
+#' The latent lognormal LPDF function
+#' 
+#' To be tested against an exposed Stan version.
+#' 
+#' @export
+latent_lognormal_lpdf <- function(y, mu, sigma, pwindow, swindow, obs_t) {
+  n <- length(y)
+  d <- y - pwindow + swindow
+  obs_time <- obs_t - pwindow
+  lpdf <- dlnorm(d, meanlog = mu, sdlog = sigma, log = TRUE)
+  lcdf <- plnorm(obs_time, meanlog = mu, sdlog = sigma, log = TRUE)
+  return(lpdf - lcdf)
 }
