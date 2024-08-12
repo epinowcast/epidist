b21da341ac518b058317d283dd9d73ebad2c7081
athowes <xwg3@cdc.gov>
2024-08-12 11:30:11 +0100

    Crawling towards prediction functionality
    Former-commit-id: aed6d47885a467465aa73b6d7c1591c979c28d9b [formerly 14f6d4434616a9841f0ac6f6806e6ecb22da70c7]
    Former-commit-id: f103f5ed2d0a1e6edeb2adff25b06a05c5e6e33c

--- inst/pred-testing.R
+++ inst/pred-testing.R
@@ -9,12 +9,5 @@ stancode <- epidist(data, fn = brms::make_stancode)
 # package precompiled somehow?
 brms::expose_functions(x = fit, vectorize = TRUE)
 
-# real latent_lognormal_lpdf(vector y, vector mu, vector sigma, vector pwindow,
-#                            vector swindow, array[] real obs_t) {
-#   int n = num_elements(y);
-#   vector[n] d = y - pwindow + swindow;
-#   vector[n] obs_time = to_vector(obs_t) - pwindow;
-#   return lognormal_lpdf(d | mu, sigma) - lognormal_lcdf(obs_time | mu, sigma);
-# }
-
 i <- 1
+
