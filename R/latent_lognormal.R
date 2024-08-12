b859f4de00a841659a659d08c5f7235a8d39def2
athowes <xwg3@cdc.gov>
2024-08-12 09:12:52 +0100

    Remove log_lik, transfer non-functions to testing script, add documentation for epred

--- R/latent_lognormal.R
+++ R/latent_lognormal.R
@@ -1,42 +1,3 @@
-data <- as_latent_individual(sim_obs)
-fit <- epidist(data)
-prep <- brms::prepare_predictions(fit)
-
-stancode <- epidist(data, fn = brms::make_stancode)
-
-# Won't be able to use expose in the package but can try with it for now
-# Need some solution for how not to write code twice? Could expose and put in
-# package precompiled somehow?
-brms::expose_functions(x = fit, vectorize = TRUE)
-
-# real latent_lognormal_lpdf(vector y, vector mu, vector sigma, vector pwindow,
-#                            vector swindow, array[] real obs_t) {
-#   int n = num_elements(y);
-#   vector[n] d = y - pwindow + swindow;
-#   vector[n] obs_time = to_vector(obs_t) - pwindow;
-#   return lognormal_lpdf(d | mu, sigma) - lognormal_lcdf(obs_time | mu, sigma);
-# }
-
-i <- 1
-
-log_lik_latent_lognormal <- function(i, prep) {
-  mu <- brms::get_dpar(prep, "mu", i = i)
-  sigma <- brms::get_dpar(prep, "sigma", i = i)
-  y <- prep$data$Y[i]
-  vreal1 <- prep$data$vreal1[i]
-  vreal2 <- prep$data$vreal2[i]
-  vreal3 <- prep$data$vreal3[i]
-  
-  # Maybe the issue here is that I need to be integrating these out...
-  # swindow_raw <- ...
-  # pwindow_raw <- ...
-  
-  # pwindow <- ...
-  # swindow <- ... 
-  # obs_t <- ...
-  latent_lognormal_lpdf(y, mu, sigma, pwindow, swindow, obs_t)
-}
-
 posterior_predict_latent_lognormal <- function(i, prep, ...) {
   mu <- brms::get_dpar(prep, "mu", i = i)
   sigma <- brms::get_dpar(prep, "sigma", i = i)
@@ -57,6 +18,14 @@ posterior_predict_latent_lognormal <- function(i, prep, ...) {
   y <- prep$data$Y[i]
 }
 
+#' Draws from the expected value of the posterior predictive distribution
+#' 
+#' See [`brms::posterior_epred`].
+#'
+#' @param prep The result of a call to [`brms::prepare_predictions`]
+#' @family postprocess
+#' @autoglobal
+#' @export
 posterior_epred_latent_lognormal <- function(prep) {
   mu <- brms::get_dpar(prep, "mu")
   sigma <- brms::get_dpar(prep, "sigma")
