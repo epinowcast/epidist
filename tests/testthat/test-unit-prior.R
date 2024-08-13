b72ea48d2b564a7992ea36ace7051d73faacbce6
athowes <xwg3@cdc.gov>
2024-08-13 11:01:23 +0100

    Document and clean up

--- tests/testthat/test-unit-prior.R
+++ tests/testthat/test-unit-prior.R
@@ -1,20 +1,8 @@
-data <- as_latent_individual(sim_obs)
-formula <- brms::bf(mu ~ 1, sigma ~ 1)
-family <- brms::lognormal()
-prior <- NULL
-fn <- brms::brm
-
-epidist_validate(data)
-epidist_family <- epidist_family(data, family)
-epidist_formula <- epidist_formula(
-  data = data, family = epidist_family, formula = formula
-)
-
-# Start of epidist_prior
-epidist_validate(data)
-default <- brms::default_prior(data, family, formula)
-model <- epidist_model_prior(data, formula)
-family <  epidist_family_prior(family, formula)
-out <- replace_brms_prior(default, model)
-out <- replace_brms_prior(out, family)
-out <- replace_brms_prior(out, prior)
+test_that("replace_brms_prior errors when passed a new prior without a match in old_prior", { # nolint: line_length_linter.
+  old_prior <- brms::prior("normal(0, 10)", class = "Intercept") +
+    brms::prior("normal(0, 10)", class = "Intercept", dpar = "sigma")
+  new_prior <- brms::prior("normal(0, 5)", class = "Intercept") +
+    brms::prior("normal(0, 5)", class = "Intercept", dpar = "sigma") +
+    brms::prior("normal(0, 5)", class = "Intercept", dpar = "shape")
+  expect_error(replace_brms_prior(old_prior, new_prior))
+})
