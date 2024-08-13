e05caefde12aabd50592d368440cdd602e59b2d8
athowes <xwg3@cdc.gov>
2024-08-13 08:58:34 +0100

    Transfer location of existing prior
    Former-commit-id: 6739307cff92577512d0c2ab4ad66b347ffea924 [formerly f93d882cf6f8a582760eeff0085c87747f9f5816]
    Former-commit-id: 49e4f6efa319c7c32c0f446bcfe1257f69ce9db9

--- R/prior.R
+++ R/prior.R
@@ -40,3 +40,40 @@ epidist_family_prior.default <- function(family, formula){
 epidist_family_prior.lognormal <- function(family, formula){
   return(NULL)
 }
+
+#' Define priors for the model
+#'
+#' We provide suggested weakly informative priors.
+#'
+#' @inheritParams epidist_prior
+#' @method epidist_prior epidist_latent_individual
+#' @family latent_individual
+#' @importFrom cli cli_inform
+#' @export
+epidist_prior.epidist_latent_individual <- function(data, family, formula,
+                                                    ...) {
+  epidist_validate(data)
+  if (identical(family$dpars, c("mu", "sigma"))) {
+    prior_mu <- brms::prior("normal(2, 0.5)", class = "Intercept")
+    prior_sigma <- brms::prior(
+      "normal(0, 0.5)", class = "Intercept", dpar = "sigma"
+    )
+    msg <- c(
+      "i" = "The following priors have been set:",
+      "*" = "normal(2, 0.5) on the intercept of distributional parameter mu",
+      "*" = "normal(0, 0.5) on the intercept of distributional parameter sigma",
+      "To alter priors, or set priors on other parameters, see ?epidist_prior."
+    )
+    cli::cli_inform(
+      message = msg, .frequency = "regularly", .frequency_id = "prior-message"
+    )
+    priors <- prior_mu + prior_sigma
+  } else {
+    cli::cli_warn(c(
+      "!" = "Priors not available for these distributional parameters.",
+      "Using the default priors from brms."
+    ))
+    priors <- NULL
+  }
+  return(priors)
+}
