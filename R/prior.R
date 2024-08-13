b7c35e3e66577542fbe564cf25cb38e4b0e2b9d0
athowes <xwg3@cdc.gov>
2024-08-13 08:52:08 +0100

    Pseudocode for the new epidist_prior set-up
    Former-commit-id: 25480b0f8d66388d12ee5fc9c57f955486835797 [formerly df7aacdeecb8f9435590454f12dd5620806ba70f]
    Former-commit-id: 0403ff47fc41df3071bdad4ba473eb13a903d14c
    Former-commit-id: cb4a726e20148777e752b1dd95c0327d02fb585d

---
+++ R/prior.R
@@ -0,0 +1,42 @@
+#' @family prior
+#' @export
+epidist_prior <- function(data, family, formula, prior) {
+  default <- brms::default_prior(data, family, formula)
+  model <- epidist_model_prior(data, formula)
+  family <  epidist_family_prior(family, formula)
+  out <- replace_brms_prior(default, model)
+  out <- replace_brms_prior(out, family)
+  out <- replace_brms_prior(out, prior)
+  return(out)
+}
+
+#' @family prior
+#' @export
+epidist_model_prior <- function(data, formula) {
+  UseMethod("epidist_model_prior")
+}
+
+#' @family prior
+#' @export
+epidist_model_prior.default <- function(data, formula){
+  return(NULL)
+}
+
+
+#' @family prior
+#' @export
+epidist_family_prior <- function(family, formula) {
+  UseMethod("epidist_family_prior")
+}
+
+#' @family prior
+#' @export
+epidist_family_prior.default <- function(family, formula){
+  return(NULL)
+}
+
+#' @family prior
+#' @export
+epidist_family_prior.lognormal <- function(family, formula){
+  return(NULL)
+}
