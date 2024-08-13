514f9b3ef8f5525f6eccd182a385a82e8483b079
athowes <xwg3@cdc.gov>
2024-08-13 10:17:31 +0100

    The fiddling required for the problem now to be the helper function
    Former-commit-id: 96a7853fd40e3e6bc7dac5b01792658a159193a3 [formerly b049e15f17bd31e70caceaef652a0344c01e21df]
    Former-commit-id: 39d1a5fc55618876ce0f55c7c186ffc517c86593

--- R/prior.R
+++ R/prior.R
@@ -1,8 +1,11 @@
+#' Define prior from defaults, model, family, user
+#'
+#' @rdname epidist_prior
 #' @family prior
 #' @export
 epidist_prior <- function(data, family, formula, prior) {
   epidist_validate(data)
-  default <- brms::default_prior(data, family, formula)
+  default <- brms::default_prior(formula, data = data)
   model <- epidist_model_prior(data, formula)
   family <  epidist_family_prior(family, formula)
   out <- replace_brms_prior(default, model)
@@ -11,40 +14,42 @@ epidist_prior <- function(data, family, formula, prior) {
   return(out)
 }
 
+#' Model specific prior
+#'
+#' @rdname epidist_model_prior
 #' @family prior
 #' @export
-epidist_model_prior <- function(data, formula) {
+epidist_model_prior <- function(data, ...) {
   UseMethod("epidist_model_prior")
 }
 
 #' @family prior
 #' @export
-epidist_model_prior.default <- function(data, formula){
+epidist_model_prior.default <- function(data, formula) {
   # Currently there are not model-specific priors
   # In future there might be, but we need to be careful about Stan code
   return(NULL)
 }
 
-
+#' Family specific prior
+#'
+#' @rdname epidist_family_prior
 #' @family prior
 #' @export
-epidist_family_prior <- function(family, formula) {
+epidist_family_prior <- function(family, ...) {
   UseMethod("epidist_family_prior")
 }
 
 #' @family prior
 #' @export
-epidist_family_prior.default <- function(family, formula){
+epidist_family_prior.default <- function(family, formula) {
   return(NULL)
 }
 
+#' @method epidist_family_prior lognormal
 #' @family prior
 #' @export
-epidist_family_prior.lognormal <- function(family, formula){
-  prior_mu <- brms::prior("normal(2, 0.5)", class = "Intercept")
-  prior_sigma <- brms::prior(
-    "normal(0, 0.5)", class = "Intercept", dpar = "sigma"
-  )
-  prior <- prior_mu + prior_sigma
-  return(prior)
+epidist_family_prior.lognormal <- function(family, formula) {
+  brms::prior("normal(2, 0.5)", class = "Intercept") + 
+    brms::prior("normal(0, 0.5)", class = "Intercept", dpar = "sigma")
 }
