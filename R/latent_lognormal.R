51894a39c3017992fcd8d660c804721e007196ae
athowes <xwg3@cdc.gov>
2024-08-26 14:51:58 +0100

    Rebase

--- R/latent_lognormal.R
+++ R/latent_lognormal.R
@@ -1,5 +1,5 @@
 #' Draws from the posterior predictive distribution
-#' 
+#'
 #' See [`brms::posterior_predict`].
 #'
 #' @param i The index of the observation to predict
@@ -7,32 +7,32 @@
 #' @family postprocess
 #' @autoglobal
 #' @export
-posterior_predict_latent_lognormal <- function(i, prep, ...) {
+posterior_predict_latent_lognormal <- function(i, prep, ...) { # nolint: object_length_linter
   mu <- brms::get_dpar(prep, "mu", i = i)
   sigma <- brms::get_dpar(prep, "sigma", i = i)
 
-  wN <- prep$data$wN[i]
+  wN <- prep$data$wN[i] # nolint: object_name_linter
   woverlap <- prep$data$woverlap[i]
   noverlap <- prep$data$noverlap[i]
-  
+
   # This assumes a certain prior on swindow_raw and pwindow_raw
   # It would be better to extract these from prep or the model in some way?
   swindow_raw <- runif(1, 0, 1)
   pwindow_raw <- runif(1, 0, 1)
-  
+
   vreal3 <- prep$data$vreal3[i]
   swindow <- vreal3 * swindow_raw
-  
+
   vreal2 <- prep$data$vreal2[i]
   pwindow <- vreal2[noverlap] * pwindow_raw[noverlap]
   if (wN) {
     pwindow[woverlap] <- swindow[woverlap] * pwindow_raw[woverlap]
   }
-  
+
   vreal1 <- prep$data$vreal1[i]
   obs_t <- vreal1
   obs_time <- obs_t - pwindow
-  
+
   d <- EnvStats::rlnormTrunc(
     1,
     meanlog = mu,
@@ -40,38 +40,45 @@ posterior_predict_latent_lognormal <- function(i, prep, ...) {
     min = 0,
     max = obs_time
   )
-  
+
   y <- d + pwindow - swindow
-  
+
   # Shouldn't this be censored to e.g. integer?
   return(y)
 }
 
 #' Draws from the expected value of the posterior predictive distribution
-#' 
+#'
 #' See [`brms::posterior_epred`].
 #'
 #' @param prep The result of a call to [`brms::prepare_predictions`]
 #' @family postprocess
 #' @autoglobal
 #' @export
-posterior_epred_latent_lognormal <- function(prep) {
+posterior_epred_latent_lognormal <- function(prep) { # nolint: object_length_linter
   mu <- brms::get_dpar(prep, "mu")
   sigma <- brms::get_dpar(prep, "sigma")
   exp(mu + sigma^2 / 2)
 }
 
 #' The latent lognormal LPDF function
-#' 
+#'
 #' To be tested against an exposed Stan version. I think that this would be
 #' used in the forthcoming `log_lik_latent_lognormal` function.
-#' 
+#'
+#' @param y ...
+#' @param mu ...
+#' @param sigma ...
+#' @param pwindow ...
+#' @param swindow ...
+#' @param obs_t ...
+#' @family postprocess
+#' @autoglobal
 #' @export
 latent_lognormal_lpdf <- function(y, mu, sigma, pwindow, swindow, obs_t) {
-  n <- length(y)
   d <- y - pwindow + swindow
   obs_time <- obs_t - pwindow
   lpdf <- dlnorm(d, meanlog = mu, sdlog = sigma, log = TRUE)
   lcdf <- plnorm(obs_time, meanlog = mu, sdlog = sigma, log.p = TRUE)
-  return(lpdf - lcdf)
+  return(sum(lpdf - lcdf))
 }
