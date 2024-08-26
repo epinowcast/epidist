8285846eba7c6b132c1ae815b86cc90afae7e1f0
athowes <xwg3@cdc.gov>
2024-08-26 15:29:08 +0100

    Now passing with bf(mu ~ 1) and no model on sigma

--- inst/vector-real.R
+++ inst/vector-real.R
@@ -6,22 +6,5 @@ set.seed(1)
 fit <- epidist(
   data = prep_obs,
   formula = brms::bf(mu ~ 1),
-  seed = 1,
-  silent = 2
-)
-
-data <- prep_obs
-formula <- brms::bf(mu ~ 1, sigma ~ 1)
-prior <- NULL
-
-epidist_validate(data)
-epidist_family <- epidist_family(data, family)
-epidist_formula <- epidist_formula(
-  data = data, family = epidist_family, formula = formula
-)
-
-# Get the dpar names
-# Only include prior bits if the dpars are there
-epidist_prior <- epidist_prior(
-  data = data, family = family, formula = epidist_formula
+  seed = 1
 )
