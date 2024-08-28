731fb7c876c6aa4f135a31a5b56abf3ea752b375
athowes <xwg3@cdc.gov>
2024-08-28 14:25:23 +0100

    Improve description of utils

--- R/utils.R
+++ R/utils.R
@@ -1,6 +1,9 @@
-#' Read in a epidist Stan code chunk
+#' Read in an `epidist` Stan code chunk
 #'
-#' @param path The path within the "stan" folder of the installed epidist
+#' This function is used to obtain Stan code chunks from the `stan/` folder of
+#' the `epidist` package. It is used within the [epidist_stancode()] function.
+#'
+#' @param path The path within the `stan/` folder of the installed `epidist`
 #' package to the Stan code chunk of interest.
 #' @return A character string containing the Stan code chunk of interest.
 #' @family utils
@@ -10,10 +13,14 @@ epidist_stan_chunk <- function(path) {
   paste(readLines(local_path), collapse = "\n")
 }
 
-#' Label a epidist Stan model with a version indicator
+#' Label a `epidist` Stan model with a version indicator
+#' 
+#' This function is used within [epidist_stancode()] to label the generated Stan
+#' code with the version of `epidist` used. To view the full Stan code for any
+#' particular `epidist` model, we recommend use of [brms::make_stancode()].
 #'
-#' @return A brms stanvar chunk containing the package version used to build
-#' the Stan code.
+#' @return A `brms` Stan chunk containing the `epidist` package version used to
+#' build the Stan code.
 #' @family utils
 #' @export
 epidist_version_stanvar <- function() {
@@ -26,7 +33,7 @@ epidist_version_stanvar <- function() {
 #'
 #' This function rounds an input `x` down to the nearest multiple of some number
 #' `f`. For example, if `f = 0.2` and `x = 1.5` then the output would be 1.4.
-#' Or, if `f = 1` then `floor_mult` behaves as `floor`.
+#' If `f = 1` then `floor_mult` behaves as `floor`.
 #'
 #' @param x A number to be rounded down
 #' @param f A number specifying the multiple to be rounded down to
