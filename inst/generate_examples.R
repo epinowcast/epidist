178b227e500387261ef21abcaa63ef31a8ec4d3f
Adam Howes <xwg3@cdc.gov>
2024-09-04 11:08:25 +0100

    Issue 248: Add prediction and log-likelihood methods for the `latent_gamma` family (#273)
    * Add quick go at these three functions for the latent_gamma family
    
    * Lint package
    
    * Rename predict tests as latent_lognormal tests
    
    * Make generate_examples reproducible and add gamma data
    
    * Add unit tests for the latent_gamma methods
    
    * Use the fit_gamma rather than fit
    
    * Upload gamma fit
    
    * Add named documentation to latent_lognormal
    
    * Reorder prep and i correctly
    
    * Use [s] not [i]! Bug
    
    * Rerun of generate test data required
    
    * Use scale parameterisation
    
    Former-commit-id: cf49d8eb45aa4c2020042b27739dcd6564125ea1 [formerly 68af55aacae61c4e961c8eda0d71f617a17687ec]
    Former-commit-id: d7725c9114d700f2b458c23dd13c77fcea81a7f9
    Former-commit-id: 7ce7e5992e1e86ef5847e2fe8dcf2ca23fe4347c

--- inst/generate_examples.R
+++ inst/generate_examples.R
@@ -1,4 +1,11 @@
 source("tests/testthat/setup.R")
+set.seed(1)
 prep_obs <- as_latent_individual(sim_obs)
-fit <- epidist(prep_obs)
-saveRDS(fit, "inst/extdata/fit.rds")
+fit <- epidist(prep_obs, seed = 1)
+prep_obs_gamma <- as_latent_individual(sim_obs_gamma)
+fit_gamma <- epidist(
+  prep_obs_gamma,
+  family = stats::Gamma(link = "log"),
+  seed = 1
+)
+saveRDS(fit_gamma, "inst/extdata/fit_gamma.rds")
