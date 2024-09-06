87ee58662943e0e049176419c1ec31952f88f6a2
athowes <xwg3@cdc.gov>
2024-09-06 13:23:44 +0100

    Move column descriptions into function roxygen

--- R/observe.R
+++ R/observe.R
@@ -1,5 +1,19 @@
 #' Observation process for primary and secondary events
 #'
+#' This function adds columns to linelist data representing an observation
+#' process with daily primary and secondary censoring, as well as right
+#' truncation. The columns added are:
+#' * `ptime_daily`: The floor of `ptime`
+#' * `ptime_lwr`: The lower bound of `ptime`. Equal to `ptime_daily`
+#' * `ptime_upr`: The upper bound of `ptime`. Equal to `ptime_lwr + 1`
+#' * `stime_daily`: The floor of `stime`
+#' * `stime_lwr`: The lower bound of `stime`. Equal to `stime_daily`
+#' * `stime_upr`: The upper bound of `stime`. Equal to `stime_lwr + 1`
+#' * `delay_daily`: Given by `stime_daily - ptime_daily`
+#' * `delay_lwr`: Given by `delay_daily - 1` (or 0 if `delay_daily < 1`)
+#' * `delay_upr`: Given by `delay_daily + 1`
+#' * `obs_at`: The maximum value of `stime`
+#'
 #' @param linelist ...
 #' @family observe
 #' @autoglobal
