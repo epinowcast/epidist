66870cb03b36a8c9d8bd2bae27b3b8d5a01976ce
athowes <xwg3@cdc.gov>
2024-09-10 10:31:29 +0100

    Run lint

--- R/utils.R
+++ R/utils.R
@@ -64,7 +64,7 @@ replace_prior <- function(old_prior, new_prior) {
   prior <- dplyr::full_join(
     old_prior, new_prior, by = cols, suffix = c("_old", "_new")
   )
-  
+
   if (any(is.na(prior$prior_old))) {
     missing_prior <- utils::capture.output(print(
       prior |>
@@ -79,10 +79,10 @@ replace_prior <- function(old_prior, new_prior) {
     )
     cli::cli_abort(message = msg)
   }
-  
+
   prior <- prior |>
     dplyr::filter(!is.na(prior_old), !is.na(prior_new)) |>
     dplyr::select(prior = prior_new, dplyr::all_of(cols), source = source_new)
-  
+
   return(prior)
 }
