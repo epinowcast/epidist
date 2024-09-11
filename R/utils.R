b61263ec393cea6782b0cd25470ca14f37782fc5
athowes <xwg3@cdc.gov>
2024-09-11 12:02:43 +0100

    Remove epidist_ from version_stanvar and stan_chunk

--- R/utils.R
+++ R/utils.R
@@ -7,7 +7,7 @@
 #' package to the Stan code chunk of interest.
 #' @return A character string containing the Stan code chunk of interest.
 #' @noRd
-epidist_stan_chunk <- function(path) {
+stan_chunk <- function(path) {
   local_path <- system.file(paste0("stan/", path), package = "epidist")
   paste(readLines(local_path), collapse = "\n")
 }
@@ -21,7 +21,7 @@ epidist_stan_chunk <- function(path) {
 #' @return A `brms` Stan chunk containing the `epidist` package version used to
 #' build the Stan code.
 #' @noRd
-epidist_version_stanvar <- function() {
+version_stanvar <- function() {
   version <- utils::packageVersion("epidist")
   comment <- paste0("// code chunks used from epidist ", version, "\n")
   brms::stanvar(scode = comment, block = "functions")
