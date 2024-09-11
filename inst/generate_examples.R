dce7edce210f5c4306681a4f362500f9e82b0f4a
athowes <xwg3@cdc.gov>
2024-09-11 20:19:07 +0100

    Update examples (and add missing save line!)

--- inst/generate_examples.R
+++ inst/generate_examples.R
@@ -2,6 +2,7 @@ source("tests/testthat/setup.R")
 set.seed(1)
 prep_obs <- as_latent_individual(sim_obs)
 fit <- epidist(prep_obs, seed = 1)
+saveRDS(fit, "inst/extdata/fit.rds")
 prep_obs_gamma <- as_latent_individual(sim_obs_gamma)
 fit_gamma <- epidist(
   prep_obs_gamma,
