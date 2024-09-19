c6d0b626b72f054acf33c0bca76235ac548a5047
Adam Howes <xwg3@cdc.gov>
2024-09-19 14:18:23 +0100

    Issue #341: Add `cli` and `checkmate` functions to namespace (#344)
    * Put checkmate functions into NAMESPACE
    
    * Put cli functions into NAMESPACE
    
    * Add assert_numeric
    
    Former-commit-id: 2de7903b8f8b420c7d2298bb4598097c5e939d85 [formerly 936c7b4d23ebac20bfbcb86d75059bfc2f27ee3a]
    Former-commit-id: 503ea7656f112a03612f9e7fc726dd9c6fe8040e

--- R/epidist-package.R
+++ R/epidist-package.R
@@ -7,5 +7,8 @@
 ## usethis namespace: start
 #' @importFrom dplyr filter select
 #' @importFrom brms bf prior
+#' @importFrom checkmate assert_data_frame assert_names assert_integer
+#'   assert_true assert_factor assert_numeric
+#' @importFrom cli cli_abort cli_inform
 ## usethis namespace: end
 NULL
