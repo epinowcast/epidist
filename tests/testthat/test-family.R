c42ea54b4829bc094f6e7901022d9ddd63d2115f
athowes <xwg3@cdc.gov>
2024-10-04 14:50:50 +0100

    Add or move tests for the family functionality

--- tests/testthat/test-family.R
+++ tests/testthat/test-family.R
@@ -1,4 +1,30 @@
-# Make this data available for other tests
 prep_obs <- as_latent_individual(sim_obs)
-family_lognormal <- epidist_family(prep_obs, family = "lognormal")
-family_gamma <- epidist_family(prep_obs, family = "gamma")
+prep_obs_gamma <- as_latent_individual(sim_obs_gamma)
+
+test_that("epidist_family with default settings produces an object of the right class", { # nolint: line_length_linter.
+  family <- epidist_family(prep_obs)
+  expect_s3_class(family, "customfamily")
+  expect_s3_class(family, "brmsfamily")
+  expect_s3_class(family, "family")
+})
+
+test_that("epidist_family gives an error when passed inappropriate family input", { # nolint: line_length_linter.
+  expect_error(epidist_family(prep_obs, family = 1))
+  expect_error(epidist_family(prep_obs, family = list()))
+})
+
+test_that("the family argument in epidist_family passes as expected for brms and stats family objects, as well as strings", { # nolint: line_length_linter.
+  family_lognormal <- epidist_family(prep_obs, family = brms::lognormal())
+  expect_equal(family_lognormal$name, "latent_lognormal")
+  family_gamma <- epidist_family(prep_obs, family = Gamma(link = "log"))
+  expect_equal(family_gamma$name, "latent_gamma")
+  string_lognormal <- epidist_family(prep_obs, family = "lognormal")
+  expect_equal(string_lognormal$name, "latent_lognormal")
+})
+
+test_that("epidist_family contains the correct reparameterisations for lognormal (no change) and gamma (a change)", { # nolint: line_length_linter.
+  family_lognormal <- epidist_family(prep_obs, family = "lognormal")
+  expect_equal(family_lognormal$reparam, c("mu", "sigma"))
+  family_gamma <- epidist_family(prep_obs, family = Gamma(link = "log"))
+  expect_equal(family_lognormal$reparam, c("shape", "shape ./ mu"))
+})
--- tests/testthat/test-latent_individual.R
+++ tests/testthat/test-latent_individual.R
@@ -55,27 +55,6 @@ test_that("epidist_validate.epidist_latent_individual returns FALSE for incorrec
   })
 })
 
-test_that("epidist_family.epidist_latent_individual with default settings produces an object of the right class", { # nolint: line_length_linter.
-  family <- epidist_family(prep_obs)
-  expect_s3_class(family, "customfamily")
-  expect_s3_class(family, "brmsfamily")
-  expect_s3_class(family, "family")
-})
-
-test_that("epidist_family.epidist_latent_individual gives an error when passed inappropriate family input", { # nolint: line_length_linter.
-  expect_error(epidist_family(prep_obs, family = 1))
-  expect_error(epidist_family(prep_obs, family = list()))
-})
-
-test_that("the family argument in epidist_family.epidist_latent_individual passes as expected for brms and stats family objects, as well as strings", { # nolint: line_length_linter.
-  family_lognormal <- epidist_family(prep_obs, family = brms::lognormal())
-  expect_equal(family_lognormal$name, "latent_lognormal")
-  family_gamma <- epidist_family(prep_obs, family = Gamma(link = "log"))
-  expect_equal(family_gamma$name, "latent_gamma")
-  string_lognormal <- epidist_family(prep_obs, family = "lognormal")
-  expect_equal(string_lognormal$name, "latent_lognormal")
-})
-
 test_that("epidist_formula.epidist_latent_individual with default settings produces a brmsformula with the correct intercept only formula", { # nolint: line_length_linter.
   form <- epidist_formula(
     prep_obs, family = family_lognormal, formula = brms::bf(mu ~ 1, sigma ~ 1)
