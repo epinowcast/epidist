37b6cd2e07c76db77d82c5e67ca5f6563265c2c5
athowes <xwg3@cdc.gov>
2024-10-14 12:14:25 +0100

    Template for add_obs_vars

---
+++ R/preprocess.R
@@ -0,0 +1,12 @@
+add_obs_vars <- function(
+  linelist
+) {
+}
+
+.rename_column <- function(df, new, old) {
+  are_char <- is.character(new) & is.character(old)
+  if (are_char) {
+    df <- dplyr::rename(df, !!new := !!old)
+  }
+  return(df)
+}
