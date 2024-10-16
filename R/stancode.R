66d753eccabbcef3399d5513c4335a5919507f32
Adam Howes <xwg3@cdc.gov>
2024-10-16 09:46:01 +0100

    Issue #382: Reorganise function locations (#389)
    * Move fitting functions into fit
    
    * Create stancode.R
    
    * Create validate.R
    
    * Remove defaults.R and generics.R and pkgdown groups
    
    * Document
    
    * Don't need this line
    
    * Standardise providing ... argument to generics
    
    * Remove whitespace
    
    * Fix R CMD CHECK warning
    
    * Return NULL by default for stancode
    
    * Add test of Stan code NULL case
    
    * Add test for stanvars class
    
    Former-commit-id: 28be50cbb06ff1329877ee99f9606be5f86f06ec [formerly 968138b7e6434efd1eab08a5ae2c8d9a422ef2cd]
    Former-commit-id: b87df5a3b287b5b2bee9431e5a0fb37e626e0479

---
+++ R/stancode.R
@@ -0,0 +1,25 @@
+#' Define model specific Stan code
+#'
+#' This function is used within [epidist()] to create any custom Stan code which
+#' is injected into `brms` via the `stanvars` argument. It is unlikely that
+#' as a user you will need this function, but we export it nonetheless to be
+#' transparent about what exactly is happening inside of a call to [epidist()].
+#'
+#' @inheritParams epidist_validate
+#' @param ... Additional arguments passed to method.
+#' @rdname epidist_stancode
+#' @family stan
+#' @export
+epidist_stancode <- function(data, ...) {
+  UseMethod("epidist_stancode")
+}
+
+#' Default method for defining model specific Stan code
+#'
+#' @inheritParams epidist_stancode
+#' @param ... Additional arguments passed to method.
+#' @family stan
+#' @export
+epidist_stancode.default <- function(data, ...) {
+  return(NULL)
+}
