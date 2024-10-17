c38f18e6a783dfaf681cffbe10432e51a9754e0a
athowes <xwg3@cdc.gov>
2024-10-17 14:27:23 +0100

    First try at complete version of direct model (without documentation)

---
+++ tests/testthat/test-int-direct_model.R
@@ -0,0 +1,36 @@
+# Note: some tests in this script are stochastic. As such, test failure may be
+# bad luck rather than indicate an issue with the code. However, as these tests
+# are reproducible, the distribution of test failures may be investigated by
+# varying the input seed. Test failure at an unusually high rate does suggest
+# a potential code issue.
+
+prep_obs <- as_direct_model(sim_obs)
+
+test_that("epidist.epidist_direct_model Stan code has no syntax errors and compiles in the default case", { # nolint: line_length_linter.
+  skip_on_cran()
+  stancode <- epidist(
+    data = prep_obs,
+    fn = brms::make_stancode,
+    output_dir = fs::dir_create(tempfile())
+  )
+  mod <- cmdstanr::cmdstan_model(
+    stan_file = cmdstanr::write_stan_file(stancode), compile = FALSE
+  )
+  expect_true(mod$check_syntax())
+  expect_no_error(mod$compile())
+})
+
+test_that("epidist.epidist_direct_model fits and the MCMC converges in the default case", { # nolint: line_length_linter.
+  # Note: this test is stochastic. See note at the top of this script
+  skip_on_cran()
+  set.seed(1)
+  fit <- epidist(
+    data = prep_obs,
+    seed = 1,
+    silent = 2,
+    output_dir = fs::dir_create(tempfile())
+  )
+  expect_s3_class(fit, "brmsfit")
+  expect_s3_class(fit, "epidist_fit")
+  expect_convergence(fit)
+})
