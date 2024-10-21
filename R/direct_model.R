a9b339ac94f9b52a4426d15c696bd28866ddea2c
Adam Howes <xwg3@cdc.gov>
2024-10-21 17:37:41 +0100

    Issue #386: Add direct model allowing pass through to `brms` (#393)
    * Template for functions required by direct model
    
    * Template for direct_model tests
    
    * First try at complete version of direct model (without documentation)
    
    * Document and lint, plus move default formula to delay ~ .
    
    * Update pkgdown
    
    * Document following merge

---
+++ R/direct_model.R
@@ -0,0 +1,68 @@
+#' Prepare direct model to pass through to `brms`
+#'
+#' @param data A `data.frame` containing line list data
+#' @family direct_model
+#' @export
+as_direct_model <- function(data) {
+  UseMethod("as_direct_model")
+}
+
+assert_direct_model_input <- function(data) {
+  assert_data_frame(data)
+  assert_names(names(data), must.include = c("case", "ptime", "stime"))
+  assert_integer(data$case, lower = 0)
+  assert_numeric(data$ptime, lower = 0)
+  assert_numeric(data$stime, lower = 0)
+}
+
+#' Prepare latent individual model
+#'
+#' This function prepares data for use with the direct model. It does this by
+#' adding columns used in the model to the `data` object provided. To do this,
+#' the `data` must already have columns for the case number (integer),
+#' (positive, numeric) times for the primary and secondary event times. The
+#' output of this function is a `epidist_direct_model` class object, which may
+#' be passed to [epidist()] to perform inference for the model.
+#'
+#' @param data A `data.frame` containing line list data
+#' @rdname as_direct_model
+#' @method as_direct_model data.frame
+#' @family direct_model
+#' @autoglobal
+#' @export
+as_direct_model.data.frame <- function(data) {
+  assert_direct_model_input(data)
+  class(data) <- c("epidist_direct_model", class(data))
+  data <- data |>
+    mutate(delay = .data$stime - .data$ptime)
+  epidist_validate(data)
+  return(data)
+}
+
+#' Validate direct model data
+#'
+#' This function checks whether the provided `data` object is suitable for
+#' running the direct model. As well as making sure that
+#' `is_direct_model()` is true, it also checks that `data` is a `data.frame`
+#' with the correct columns.
+#'
+#' @param data A `data.frame` containing line list data
+#' @param ... ...
+#' @method epidist_validate epidist_direct_model
+#' @family direct_model
+#' @export
+epidist_validate.epidist_direct_model <- function(data, ...) {
+  assert_true(is_direct_model(data))
+  assert_direct_model_input(data)
+  assert_names(names(data), must.include = c("case", "ptime", "stime", "delay"))
+  assert_numeric(data$delay, lower = 0)
+}
+
+#' Check if data has the `epidist_direct_model` class
+#'
+#' @param data A `data.frame` containing line list data
+#' @family latent_individual
+#' @export
+is_direct_model <- function(data) {
+  inherits(data, "epidist_direct_model")
+}
