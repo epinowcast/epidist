ff87982aa11e1059acd798885541e3e96aff3ce6
athowes <xwg3@cdc.gov>
2024-10-22 09:41:03 +0100

    Refactor validation functionality

--- R/validate.R
+++ R/validate.R
@@ -1,24 +1,33 @@
-#' Validate a data object for use with [epidist()]
-#'
-#' This function validates that the provided `data` is suitable to run a
-#' particular `epidist` model. This may include checking the class of `data`,
-#' and that it contains suitable columns.
-#'
 #' @inheritParams epidist
 #' @family validate
 #' @export
-epidist_validate <- function(data, ...) {
-  UseMethod("epidist_validate")
+epidist_validate_data <- function(data, ...) {
+  UseMethod("epidist_validate_data")
 }
 
-#' Default method for data validation
-#'
 #' @inheritParams epidist
 #' @family validate
 #' @export
-epidist_validate.default <- function(data, ...) {
+epidist_validate_data.default <- function(data, ...) {
   cli_abort(
-    "No epidist_validate method implemented for the class ", class(data), "\n",
-    "See methods(epidist_validate) for available methods"
+    "No epidist_validate_data method implemented for the class ", class(data),
+    "\n", "See methods(epidist_validate_data) for available methods"
+  )
+}
+
+#' @inheritParams epidist
+#' @family validate
+#' @export
+epidist_validate_model <- function(data, ...) {
+  UseMethod("epidist_validate_data")
+}
+
+#' @inheritParams epidist
+#' @family validate
+#' @export
+epidist_validate_model.default <- function(data, ...) {
+  cli_abort(
+    "No epidist_validate_model method implemented for the class ", class(data),
+    "\n", "See methods(epidist_validate_model) for available methods"
   )
 }
