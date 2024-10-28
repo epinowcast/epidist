13c715f68ce539a646f85de507a3615437d093f1
athowes <xwg3@cdc.gov>
2024-10-28 13:48:07 +0000

    Clear up the direct model file a bit

--- R/direct_model.R
+++ R/direct_model.R
@@ -15,17 +15,6 @@ assert_direct_model_input <- function(data) {
   assert_numeric(data$stime, lower = 0)
 }
 
-#' Prepare latent individual model
-#'
-#' This function prepares data for use with the direct model. It does this by
-#' adding columns used in the model to the `data` object provided. To do this,
-#' the `data` must already have columns for the case number (integer),
-#' (positive, numeric) times for the primary and secondary event times. The
-#' output of this function is a `epidist_direct_model` class object, which may
-#' be passed to [epidist()] to perform inference for the model.
-#'
-#' @param data A `data.frame` containing line list data
-#' @rdname as_direct_model
 #' @method as_direct_model data.frame
 #' @family direct_model
 #' @autoglobal
@@ -39,15 +28,6 @@ as_direct_model.data.frame <- function(data) {
   return(data)
 }
 
-#' Validate direct model data
-#'
-#' This function checks whether the provided `data` object is suitable for
-#' running the direct model. As well as making sure that
-#' `is_direct_model()` is true, it also checks that `data` is a `data.frame`
-#' with the correct columns.
-#'
-#' @param data A `data.frame` containing line list data
-#' @param ... ...
 #' @method epidist_validate_model epidist_direct_model
 #' @family direct_model
 #' @export
