61b21808bcee499cb5a21ba8ff220628410991e4
athowes <xwg3@cdc.gov>
2024-10-31 11:02:02 +0000

    Fixes to tests

--- tests/testthat/test-postprocess.R
+++ tests/testthat/test-postprocess.R
@@ -39,9 +39,8 @@ test_that("predict_delay_parameters accepts newdata arguments and prediction by 
   expect_equal(length(unique(pred_sex$draw)), summary(fit_sex)$total_ndraws)
 
   pred_sex_summary <- pred_sex |>
-    dplyr::mutate(index = as.factor(index)) |>
     dplyr::left_join(
-      dplyr::select(prep_obs_sex, index = row_id, sex),
+      dplyr::select(prep_obs_sex, index = .row_id, sex),
       by = "index"
     ) |>
     dplyr::group_by(sex) |>
