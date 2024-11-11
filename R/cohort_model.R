810423ac22dc0d85f1029c486fb944c49319c4b1
athowes <xwg3@cdc.gov>
2024-11-11 09:44:42 +0000

    Fix to previous commit (name as cohort_model)

--- R/cohort_model.R
+++ R/cohort_model.R
@@ -3,7 +3,7 @@
 #' @param data A `data.frame` containing line list data
 #' @family cohort_model
 #' @export
-as_direct_model <- function(data) {
+as_cohort_model <- function(data) {
   UseMethod("as_cohort_model")
 }
 
@@ -14,14 +14,14 @@ assert_cohort_model_input <- function(data) {
 #' Prepare cohort model
 #'
 #' @param data A `data.frame` containing line list data
-#' @rdname as_direct_model
-#' @method as_direct_model data.frame
+#' @rdname as_cohort_model
+#' @method as_cohort_model data.frame
 #' @family cohort_model
 #' @autoglobal
 #' @export
-as_direct_model.data.frame <- function(data) {
-  assert_direct_model_input(data)
-  class(data) <- c("epidist_direct_model", class(data))
+as_cohort_model.data.frame <- function(data) {
+  assert_cohort_model_input(data)
+  class(data) <- c("epidist_cohort_model", class(data))
   data <- data |>
     mutate(delay = .data$stime - .data$ptime)
   epidist_validate(data)
@@ -45,6 +45,6 @@ epidist_validate.epidist_cohort_model <- function(data, ...) {
 #' @param data A `data.frame` containing line list data
 #' @family cohort_model
 #' @export
-is_direct_model <- function(data) {
-  inherits(data, "epidist_direct_model")
+is_cohort_model <- function(data) {
+  inherits(data, "epidist_cohort_model")
 }
