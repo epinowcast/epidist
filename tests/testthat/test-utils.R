f04bd301e5db0b1c2188c8d5cb6bc5f3874ab07e
athowes <xwg3@cdc.gov>
2024-11-15 14:40:05 +0000

    Was on wrong test

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,6 +1,6 @@
 test_that(".floor_mult works as expected, including with one and zero as f", { # nolint: line_length_linter.
-  expect_identical(.floor_mult(1.5, 0.2), 1.4)
-  expect_equal(.floor_mult(1.5, 1), floor(1.5), tolerance = 1e-08)
+  expect_equal(.floor_mult(1.5, 0.2), 1.4, tolerance = 1e-08)
+  expect_identical(.floor_mult(1.5, 1), floor(1.5))
   expect_identical(.floor_mult(1.5, 0), 1.5)
   expect_error(.floor_mult(1.5, -1))
 })
