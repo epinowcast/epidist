3f7ecabae01c9a2f94b9722cad31558410f6ba23
athowes <xwg3@cdc.gov>
2024-11-15 14:29:32 +0000

    Pass these more stringent tests

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,6 +1,6 @@
 test_that(".floor_mult works as expected, including with one and zero as f", { # nolint: line_length_linter.
   expect_identical(.floor_mult(1.5, 0.2), 1.4)
-  expect_identical(.floor_mult(1.5, 1), floor(1.5))
+  expect_equal(.floor_mult(1.5, 1), floor(1.5), tolerance = 1e-08)
   expect_identical(.floor_mult(1.5, 0), 1.5)
   expect_error(.floor_mult(1.5, -1))
 })
@@ -12,7 +12,7 @@ test_that(".replace_prior successfully replaces priors", { # nolint: line_length
     brms::prior("normal(0, 5)", class = "Intercept", dpar = "sigma")
   prior <- .replace_prior(old_prior, new_prior)
   expect_identical(prior$prior, c("normal(0, 5)", "normal(0, 5)"))
-  expect_identical(nrow(prior), 2)
+  expect_identical(as.double(nrow(prior)), 2)
   expect_s3_class(prior, "brmsprior")
   expect_s3_class(prior, "data.frame")
 })
