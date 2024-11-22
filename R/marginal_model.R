cff3176e2c8e88bbe9fa819c514714d769445037
athowes <xwg3@cdc.gov>
2024-11-22 14:16:43 +0000

    Move to marginal model name and lint

---
+++ R/marginal_model.R
@@ -0,0 +1,55 @@
+#' Prepare marginal model to pass through to `brms`
+#'
+#' @param data A `data.frame` containing line list data
+#' @family marginal_model
+#' @export
+as_epidist_marginal_model <- function(data) {
+  UseMethod("as_epidist_marginal_model")
+}
+
+#' The marginal model method for `epidist_linelist_data` objects
+#'
+#' @param data An `epidist_linelist_data` object
+#' @method as_epidist_marginal_model epidist_linelist_data
+#' @family marginal_model
+#' @autoglobal
+#' @export
+as_epidist_marginal_model.epidist_linelist_data <- function(data) {
+  assert_epidist(data)
+
+  data <- data |>
+    mutate(delay = .data$stime_lwr - .data$ptime_lwr)
+
+  data <- new_epidist_marginal_model(data)
+  assert_epidist(data)
+  return(data)
+}
+
+#' Class constructor for `epidist_marginal_model` objects
+#'
+#' @param data A data.frame to convert
+#' @returns An object of class `epidist_marginal_model`
+#' @family marginal_model
+#' @export
+new_epidist_marginal_model <- function(data) {
+  class(data) <- c("epidist_marginal_model", class(data))
+  return(data)
+}
+
+#' @method assert_epidist epidist_marginal_model
+#' @family marginal_model
+#' @export
+assert_epidist.epidist_marginal_model <- function(data, ...) {
+  assert_data_frame(data)
+  assert_names(names(data), must.include = "delay")
+  assert_numeric(data$delay, lower = 0)
+}
+
+#' Check if data has the `epidist_marginal_model` class
+#'
+#' @param data A `data.frame` containing line list data
+#' @family marginal_model
+#' @export
+is_epidist_marginal_model <- function(data) {
+  inherits(data, "epidist_marginal_model")
+}
