4024abae4066478de9ed9194827c3b54d11666f7
Sam <s.e.abbott12@gmail.com>
2024-12-02 16:47:40 +0000

    use the right transform data keyword

--- R/transform_data.R
+++ R/transform_data.R
@@ -9,7 +9,7 @@
 #' @param family A description of the response distribution and link function to
 #' be used in the model created using [epidist_family()].
 #' @param formula A formula object created using [epidist_formula()].
-#' @family transform
+#' @family transform_data
 #' @export
 epidist_transform_data <- function(data, family, formula, ...) {
   assert_epidist(data)
@@ -21,7 +21,7 @@ epidist_transform_data <- function(data, family, formula, ...) {
 #'
 #' @inheritParams epidist_transform_data
 #' @rdname epidist_transform_data_model
-#' @family transform
+#' @family transform_data
 #' @export
 epidist_transform_data_model <- function(data, family, formula, ...) {
   UseMethod("epidist_transform_data_model")
@@ -30,7 +30,7 @@ epidist_transform_data_model <- function(data, family, formula, ...) {
 #' Default method for transforming data for a model
 #'
 #' @inheritParams epidist_transform_data_model
-#' @family transform
+#' @family transform_data
 #' @export
 epidist_transform_data_model.default <- function(data, family, formula, ...) {
   return(data)
