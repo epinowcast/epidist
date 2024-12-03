69c777de26a343c98cf8954275407cd7235ddc0a
Adam Howes <xwg3@cdc.gov>
2024-12-03 13:22:46 +0000

    Issue #221: Add marginal model (#426)
    * Add cohort model template
    
    * Fix to previous commit (name as cohort_model)
    
    * Generate simulated cohort data
    
    * Add unweighted and weighted direct models
    
    * Thinking about custom family for pcd function
    
    * functions component of stanvars
    
    * Add transformed parameters for cohort model
    
    * Progress on implementing PCD model
    
    * Set q as vector
    
    * Get rid of params input
    
    * This would work, apart from it's the CDF. For the PMF need to import many primarycensored functions..
    
    * Almost working with "import all functions" strategy
    
    * Wrap up on attempt
    
    * Rename to marginal model
    
    * Move towards single wrap function with others imported
    
    * Just running into some C++ errors..
    
    * This doesn't change anything
    
    * Move to marginal model name and lint
    
    * Create aggregate data inside model conversion function for now
    
    * First draft on moving marginal_model into functions
    
    * Run document
    
    * Tests working up to valid Stan code
    
    * Regex version of marginal model
    
    * Use prep_marginal_obs
    
    * Improve assert for marginal model
    
    * Add pkgdown and document
    
    * Clean up scratch implementation
    
    * remove scratch file
    
    * update data format, formula, and family
    
    * update stan code
    
    * basic working version
    
    * add transformm data methods
    
    * start exploring the ebola example
    
    * add a helper to find meaningful relative_obs_times
    
    * get the full ebola vignette working with new variable requirements
    
    * improve return messages
    
    * update approx vignette
    
    * get ebole vignette passing by checking pp and related inputs
    
    * add marginal model integration tests
    
    * expand post processing tests
    
    * add marginal model
    
    * use the right transform data keyword
    
    * add ... pass through to make constructors correct
    
    * fix .summarise_n_by_formula test so error message is as expected
    
    * drop not required .row_id
    
    * check using ... properly
    
    * make the progress messages prettier for reducing data complexit:
    
    * check post process tests again
    
    * add a test for the specific transform data method
    
    * add some tests for the generic transform data method
    
    * put transform data tests in the correct folder
    
    * add a news update
    
    * change vignette language to talk about marginal model
    
    * update the FAQ to use the marginal variables
    
    * call it transformed_data not trans_data
    
    * change the error message to make it clear its a epidist limitation
    
    * update stan docs
    
    * Update NEWS.md
    
    * Update NEWS.md
    
    Co-authored-by: Adam Howes <xwg3@cdc.gov>
    
    * Update NEWS.md
    
    Co-authored-by: Adam Howes <xwg3@cdc.gov>
    
    * Update inst/stan/latent_model/functions.stan
    
    Co-authored-by: Adam Howes <xwg3@cdc.gov>
    
    * Update setup.R
    
    ---------
    
    Co-authored-by: Sam <s.e.abbott12@gmail.com>
    Former-commit-id: 0c41c481bcd9767252b562e8fa704e6b43995070 [formerly 267310c44261cc0e4cae488103b7f2028b4f0153]
    Former-commit-id: 277996547d66fd5d6260710b620f67f7ed3475d6
    Former-commit-id: 61dac3bfb515935da76fad795631a17a5c7f0404

---
+++ tests/testthat/test-marginal_model.R
@@ -0,0 +1,107 @@
+test_that("as_epidist_marginal_model.epidist_linelist_data with default settings an object with the correct classes", { # nolint: line_length_linter.
+  prep_marginal_obs <- as_epidist_marginal_model(sim_obs)
+  expect_s3_class(prep_marginal_obs, "data.frame")
+  expect_s3_class(prep_marginal_obs, "epidist_marginal_model")
+})
+
+test_that("as_epidist_marginal_model.epidist_linelist_data errors when passed incorrect inputs", { # nolint: line_length_linter.
+  expect_error(as_epidist_marginal_model(list()))
+  expect_error(as_epidist_marginal_model(sim_obs[, 1]))
+})
+
+# Make this data available for other tests
+family_lognormal <- epidist_family(prep_marginal_obs, family = lognormal())
+
+test_that("is_epidist_marginal_model returns TRUE for correct input", { # nolint: line_length_linter.
+  expect_true(is_epidist_marginal_model(prep_marginal_obs))
+  expect_true({
+    x <- list()
+    class(x) <- "epidist_marginal_model"
+    is_epidist_marginal_model(x)
+  })
+})
+
+test_that("is_epidist_marginal_model returns FALSE for incorrect input", { # nolint: line_length_linter.
+  expect_false(is_epidist_marginal_model(list()))
+  expect_false({
+    x <- list()
+    class(x) <- "epidist_marginal_model_extension"
+    is_epidist_marginal_model(x)
+  })
+})
+
+test_that("assert_epidist.epidist_marginal_model doesn't produce an error for correct input", { # nolint: line_length_linter.
+  expect_no_error(assert_epidist(prep_marginal_obs))
+})
+
+test_that("assert_epidist.epidist_marginal_model returns FALSE for incorrect input", { # nolint: line_length_linter.
+  expect_error(assert_epidist(list()))
+  expect_error(assert_epidist(prep_marginal_obs[, 1]))
+  expect_error({
+    x <- list()
+    class(x) <- "epidist_marginal_model"
+    assert_epidist(x)
+  })
+})
+
+test_that("epidist_stancode.epidist_marginal_model produces valid stanvars", { # nolint: line_length_linter.
+  epidist_family <- epidist_family(prep_marginal_obs)
+  epidist_formula <- epidist_formula(
+    prep_marginal_obs, epidist_family,
+    formula = bf(mu ~ 1)
+  )
+  stancode <- epidist_stancode(
+    prep_marginal_obs,
+    family = epidist_family, formula = epidist_formula
+  )
+  expect_s3_class(stancode, "stanvars")
+})
+
+test_that("epidist_transform_data_model.epidist_marginal_model correctly transforms data and messages", { # nolint: line_length_linter.
+  family <- epidist_family(prep_marginal_obs, family = lognormal())
+  formula <- epidist_formula(
+    prep_marginal_obs,
+    formula = bf(mu ~ 1),
+    family = family
+  )
+  expect_no_message(
+    expect_message(
+      expect_message(
+        expect_message(
+          epidist_transform_data_model(
+            prep_marginal_obs,
+            family = family,
+            formula = formula
+          ),
+          "Reduced from 500 to 144 rows."
+        ),
+        "Data summarised by unique combinations of:"
+      ),
+      "Model variables"
+    )
+  )
+
+  family <- epidist_family(prep_marginal_obs, family = lognormal())
+  formula <- epidist_formula(
+    prep_marginal_obs,
+    formula = bf(mu ~ 1 + ptime_lwr),
+    family = family
+  )
+  expect_message(
+    expect_message(
+      expect_message(
+        expect_message(
+          epidist_transform_data_model(
+            prep_marginal_obs,
+            family = family,
+            formula = formula
+          ),
+          "Reduced from 500 to 144 rows."
+        ),
+        "Data summarised by unique combinations of:"
+      ),
+      "Model variables"
+    ),
+    "ptime_lwr"
+  )
+})
