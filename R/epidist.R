ce41f4222a69f5589740fe6a533e69654b8400fc
Adam Howes <xwg3@cdc.gov>
2024-12-03 13:22:46 +0000

    Issue #221: Add marginal model (#426)
    * Add cohort model template
    
    * Fix to previous commit (name as cohort_model)
    
    * Generate simulated cohort data
    
    * Add unweighted and weighted direct models
    
    * Thinking about custom family for pcd function
    
    * functions component of stanvars
    
    * Add transformed parameters for cohort model
    
    * Progress on implementing PCD model
    
    * Set q as vector
    
    * Get rid of params input
    
    * This would work, apart from it's the CDF. For the PMF need to import many primarycensored functions..
    
    * Almost working with "import all functions" strategy
    
    * Wrap up on attempt
    
    * Rename to marginal model
    
    * Move towards single wrap function with others imported
    
    * Just running into some C++ errors..
    
    * This doesn't change anything
    
    * Move to marginal model name and lint
    
    * Create aggregate data inside model conversion function for now
    
    * First draft on moving marginal_model into functions
    
    * Run document
    
    * Tests working up to valid Stan code
    
    * Regex version of marginal model
    
    * Use prep_marginal_obs
    
    * Improve assert for marginal model
    
    * Add pkgdown and document
    
    * Clean up scratch implementation
    
    * remove scratch file
    
    * update data format, formula, and family
    
    * update stan code
    
    * basic working version
    
    * add transformm data methods
    
    * start exploring the ebola example
    
    * add a helper to find meaningful relative_obs_times
    
    * get the full ebola vignette working with new variable requirements
    
    * improve return messages
    
    * update approx vignette
    
    * get ebole vignette passing by checking pp and related inputs
    
    * add marginal model integration tests
    
    * expand post processing tests
    
    * add marginal model
    
    * use the right transform data keyword
    
    * add ... pass through to make constructors correct
    
    * fix .summarise_n_by_formula test so error message is as expected
    
    * drop not required .row_id
    
    * check using ... properly
    
    * make the progress messages prettier for reducing data complexit:
    
    * check post process tests again
    
    * add a test for the specific transform data method
    
    * add some tests for the generic transform data method
    
    * put transform data tests in the correct folder
    
    * add a news update
    
    * change vignette language to talk about marginal model
    
    * update the FAQ to use the marginal variables
    
    * call it transformed_data not trans_data
    
    * change the error message to make it clear its a epidist limitation
    
    * update stan docs
    
    * Update NEWS.md
    
    * Update NEWS.md
    
    Co-authored-by: Adam Howes <xwg3@cdc.gov>
    
    * Update NEWS.md
    
    Co-authored-by: Adam Howes <xwg3@cdc.gov>
    
    * Update inst/stan/latent_model/functions.stan
    
    Co-authored-by: Adam Howes <xwg3@cdc.gov>
    
    * Update setup.R
    
    ---------
    
    Co-authored-by: Sam <s.e.abbott12@gmail.com>

--- R/epidist.R
+++ R/epidist.R
@@ -38,16 +38,20 @@ epidist <- function(data, formula = mu ~ 1,
   epidist_formula <- epidist_formula(
     data = data, family = epidist_family, formula = formula
   )
+  transformed_data <- epidist_transform_data(
+    data, epidist_family, epidist_formula
+  )
   epidist_prior <- epidist_prior(
-    data = data, family = epidist_family, formula = epidist_formula, prior,
+    data = transformed_data, family = epidist_family,
+    formula = epidist_formula, prior,
     merge = merge_priors
   )
   epidist_stancode <- epidist_stancode(
-    data = data, family = epidist_family, formula = epidist_formula
+    data = transformed_data, family = epidist_family, formula = epidist_formula
   )
   fit <- fn(
     formula = epidist_formula, family = epidist_family, prior = epidist_prior,
-    stanvars = epidist_stancode, data = data, ...
+    stanvars = epidist_stancode, data = transformed_data, ...
   )
   class(fit) <- c(class(fit), "epidist_fit")
   return(fit)
---
+++ R/transform_data.R
@@ -0,0 +1,37 @@
+#' Transform data for an epidist model
+#'
+#' This function is used within [epidist()] to transform data before passing to
+#' `brms`. It is unlikely that as a user you will need this function, but we
+#' export it nonetheless to be transparent about what happens inside of a call
+#' to [epidist()].
+#'
+#' @inheritParams epidist
+#' @param family A description of the response distribution and link function to
+#' be used in the model created using [epidist_family()].
+#' @param formula A formula object created using [epidist_formula()].
+#' @family transform_data
+#' @export
+epidist_transform_data <- function(data, family, formula, ...) {
+  assert_epidist(data)
+  data <- epidist_transform_data_model(data, family, formula)
+  return(data)
+}
+
+#' The model-specific parts of an `epidist_transform_data()` call
+#'
+#' @inheritParams epidist_transform_data
+#' @rdname epidist_transform_data_model
+#' @family transform_data
+#' @export
+epidist_transform_data_model <- function(data, family, formula, ...) {
+  UseMethod("epidist_transform_data_model")
+}
+
+#' Default method for transforming data for a model
+#'
+#' @inheritParams epidist_transform_data_model
+#' @family transform_data
+#' @export
+epidist_transform_data_model.default <- function(data, family, formula, ...) {
+  return(data)
+}
