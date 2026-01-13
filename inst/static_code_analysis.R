2eb34cdea9504720f86cc3283911860dab6bbb75
Sam Abbott <s.e.abbott12@gmail.com>
2026-01-13 11:06:45 +0000

    Delete inst/static_code_analysis.R

--- inst/static_code_analysis.R
+++
@@ -1,7 +0,0 @@
-library(CodeDepends)
-
-gg <- CodeDepends::makeCallGraph("package:epidist")
-gg <- layoutGraph(gg, layoutType = "neato")
-graph.par(list(nodes = list(fontsize = 40)))
-
-renderGraph(gg)
