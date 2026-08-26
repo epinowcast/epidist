3ac580fc48f06103f4ca97b46e3672763e1d1c34
Sam Abbott (bot) <signin@samabbott.co.uk>
2026-08-26 11:52:52 +0100

    ci: pin precommit hooks to a revision with digest 0.6.39 (#608)

--- tests/testthat/helper-functions.R
+++ tests/testthat/helper-functions.R
@@ -1,28 +1,28 @@
 on_ci <- function() {
-  isTRUE(as.logical(Sys.getenv("CI")))
+  return(isTRUE(as.logical(Sys.getenv("CI"))))
 }
 
 not_on_cran <- function() {
-  identical(Sys.getenv("NOT_CRAN"), "true")
+  return(identical(Sys.getenv("NOT_CRAN"), "true"))
 }
 
 skip_on_local <- function() {
   if (on_ci()) {
     return(invisible(TRUE))
   }
-  testthat::skip("Not on CI")
+  return(testthat::skip("Not on CI"))
 }
 
 as_string_formula <- function(formula) {
-  form <- paste(deparse(formula), collapse = " ")
+  form <- deparse1(formula, collapse = " ")
   form <- gsub("\\s+", " ", form, perl = FALSE)
   return(form)
 }
 
 extract_normal_parameters_brms <- function(prior) {
   pattern <- "normal\\(([^,]+), ([^\\)]+)\\)" # nolint
-  match <- regmatches(prior, regexec(pattern, prior))
-  mean <- as.numeric(match[[1]][2])
-  sd <- as.numeric(match[[1]][3])
-  return(list(mean = mean, sd = sd))
+  matched <- regmatches(prior, regexec(pattern, prior))
+  prior_mean <- as.numeric(matched[[1]][2])
+  prior_sd <- as.numeric(matched[[1]][3])
+  return(list(mean = prior_mean, sd = prior_sd))
 }
